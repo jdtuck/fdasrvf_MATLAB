24e6e8e77589273aae11c728f525f996c2e3e22f
J. Derek Tucker <jdtuck@sandia.gov>
2017-04-14 12:24:13 -0600

    update randomGamma to use subfunctions

--- randomGamma.m
+++ randomGamma.m
@@ -1,35 +1,27 @@
 function rgam = randomGamma(gam,num)
 
-[mu,psi,vec] = SqrtMean(gam);
+[mu,~,vec] = SqrtMean(gam);
 
 K = cov(vec);
-[U,S,V] = svd(K);
+[U,S,~] = svd(K);
 Sig = diag(S);
 n = 5;
-T = length(vec) + 1;
-
-% Random Sampling
-% figure(12); clf; 
-% axes('FontSize',20);
-% hold on;
+T = length(vec);
+time = linspace(0,1,T);
+time = time(:);
 
 vm = mean(vec);
+rgam = zeros(num, length(gam));
 for k=1:num
-    
+
     a = randn(1,n);
     v = zeros(size(vm));
     for i=1:n
         v = v + a(i)*sqrt(Sig(i))*U(:,i)';
     end
-    vn = norm(v)/sqrt(T);
-    psi = cos(vn)*mu + sin(vn)*v/vn;
+    psi = exp_map(mu, v);
 
-    gam0 = [0 cumsum(psi.*psi)]/T;
+    gam0 = cumtrapz(time,psi.^2);
     rgam(k,:) = (gam0-gam0(1))/(gam0(end)-gam0(1));  % slight change on scale
 
-    % plot([1:T]/T,rgam(k,:),'LineWidth',2);
 end
-% axis equal;
-% axis([0 1 0 1]);
-% grid minor;
-
