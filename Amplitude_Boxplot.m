6e20cfd2b7097c7a043c584ba25bb6d1f8aba35e
Derek Tucker <jdtuck@sandia.gov>
2017-09-07 08:54:04 -0600

    update amplitude boxplot to do quantiles

--- Amplitude_Boxplot.m
+++ Amplitude_Boxplot.m
@@ -1,25 +1,15 @@
-function [median_y, Q1, Q3, minn, maxx, outlier_index] = Amplitude_Boxplot(f_tilde, f_median, q_tilde, q_median, t, k_a)
+function [median_y, Q1, Q3, Q1a, Q3a, minn, maxx, outlier_index] = Amplitude_Boxplot(f_tilde, f_median, q_tilde, q_median, t, alpha, k_a, figs)
 % k_a: outlier cutoff constant for amplitude component
+% alpha: quantile value
 
 [M, N] = size(f_tilde);
 lambda = 0.5;
-figs = 0;
-
-if figs==1
-    figure(300); clf;
-        plot(t, f_tilde, 'LineWidth',2);
-        xlim([t(1) t(end)]);
-        ylim auto;
-        set(gca,'FontSize',19);
-        figax=axis;
-        ti = get(gca,'TightInset');
-        set(gca,'Position',[ti(1) ti(2) 1-ti(3)-ti(1) 1-ti(4)-ti(2)]);
-end
 
 % amplitude median
 median_y = f_median;
 
 % compute amplitude distances
+dy = zeros(1,N);
 for i = 1:N
     dy(i) = sqrt(trapz(t,(q_median-q_tilde(:,i)).^2));
 end
@@ -28,6 +18,8 @@ CR_50 = dy_ordering(1:ceil(N/2));       % 50% Central Region
 m = max(dy(CR_50));                     % Maximal amplitude distance within 50% Central Region
 
 % identify amplitude quartiles
+angle = zeros(length(CR_50), length(CR_50));
+energy = zeros(length(CR_50), length(CR_50));
 for i = 1:(length(CR_50)-1)
     for j = (i+1):length(CR_50)
         q1 = q_tilde(:,CR_50(i)) - q_median;
@@ -38,8 +30,8 @@ for i = 1:(length(CR_50)-1)
         energy(i,j) = (1-lambda) * (dy(CR_50(i))/m + dy(CR_50(j))/m) - lambda * (angle(i,j) + 1);
     end
 end
-[maxval, maxloc] = max(energy(:));
-[maxloc_row maxloc_col] = ind2sub(size(energy), maxloc);
+[~, maxloc] = max(energy(:));
+[maxloc_row, maxloc_col] = ind2sub(size(energy), maxloc);
 
 Q1_index = CR_50(maxloc_row);
 Q3_index = CR_50(maxloc_col);
@@ -47,6 +39,30 @@ Q1_q = q_tilde(:,Q1_index);
 Q3_q = q_tilde(:,Q3_index);
 Q1 = f_tilde(:,Q1_index);
 Q3 = f_tilde(:,Q3_index);
+
+% identify amplitude quantiles
+[~, dy_ordering] = sort(dy);
+CR_alpha = dy_ordering(1:round(N*(1-alpha)));       % (1-alpha)% Central Region
+m = max(dy(CR_alpha));                     % Maximal amplitude distance within 50% Central Region
+for i = 1:(length(CR_alpha)-1)
+    for j = (i+1):length(CR_alpha)
+        q1 = q_tilde(:,CR_alpha(i)) - q_median;
+        q3 = q_tilde(:,CR_alpha(j)) - q_median;
+        q1=q1/sqrt(trapz(t,q1.^2));     % normalize to unit 1
+        q3=q3/sqrt(trapz(t,q3.^2));
+        angle(i,j)=trapz(t,q1.*q3);
+        energy(i,j) = (1-lambda) * (dy(CR_alpha(i))/m + dy(CR_alpha(j))/m) - lambda * (angle(i,j) + 1);
+    end
+end
+[~, maxloc] = max(energy(:));
+[maxloc_row, maxloc_col] = ind2sub(size(energy), maxloc);
+
+Q1a_index = CR_alpha(maxloc_row);
+Q3a_index = CR_alpha(maxloc_col);
+Q1a_q = q_tilde(:,Q1a_index);
+Q3a_q = q_tilde(:,Q3a_index);
+Q1a = f_tilde(:,Q1a_index);
+Q3a = f_tilde(:,Q3a_index);
 
 % compute amplitude whiskers
 IQR = dy(Q1_index)+dy(Q3_index);
@@ -57,55 +73,6 @@ lower_q = Q1_q + k_a * IQR * v1 / sqrt(trapz(t,v1.^2));
 upper = cumtrapz(t,upper_q.*abs(upper_q));
 lower = cumtrapz(t,lower_q.*abs(lower_q));
 
-if figs == 1
-    figure(301); clf; 
-        plot(t, f_median, 'black','linewidth', 2); 
-        hold on;
-        plot(t, Q1, 'blue','linewidth', 2); 
-        plot(t, Q3,'green', 'linewidth', 2);
-        plot(t,lower,'red','linewidth',2);
-        plot(t,upper,'magenta','linewidth',2)
-        xlim([t(1) t(end)]);
-        ylim auto;
-        set(gca,'FontSize',18);
-        ti = get(gca,'TightInset');
-        set(gca,'Position',[ti(1) ti(2) 1-ti(3)-ti(1) 1-ti(4)-ti(2)]); 
-                 
-    % surface plot
-    s = linspace(0,1,100);
-    Fs1(:,1) = (1-s(1)) * lower + s(1) * Q1; 
-    for j=2:100
-        Fs1(:,j) = (1-s(j)) * lower + s(j) * Q1; 
-        Fs1(:,99+j) = (1-s(j)) * Q1 + s(j) * f_median; 
-        Fs1(:,198+j) = (1-s(j)) * f_median + s(j) * Q3; 
-        Fs1(:,297+j) = (1-s(j)) * Q3 + s(j) * upper; 
-    end
-    d1=sqrt(trapz(t,(q_median-Q1_q).^2));
-    dl=sqrt(trapz(t,(Q1_q-lower_q).^2));
-    d3=sqrt(trapz(t,(q_median-Q3_q).^2));
-    du=sqrt(trapz(t,(Q3_q-upper_q).^2));
-    part1=linspace(-d1-dl,-d1,100);
-    part2=linspace(-d1,0,100);
-    part3=linspace(0,d3,100);
-    part4=linspace(d3,d3+du,100);
-    allparts=[part1,part2(2:100),part3(2:100),part4(2:100)];
-    [U,V]=meshgrid(t,allparts);
-
-    figure(302); clf;
-        surf(U',V',Fs1);
-        hold on;
-        shading flat;
-        light;    
-        plot3(linspace(0,1,M),zeros(1,M),f_median,'k','LineWidth',3)
-        plot3(linspace(0,1,M),repmat(-d1,M,1),Q1,'b','LineWidth',3)
-        plot3(linspace(0,1,M),repmat(-d1-dl,M,1),lower,'r','LineWidth',3)
-        plot3(linspace(0,1,M),repmat(d3,M,1),Q3,'g','LineWidth',3)
-        plot3(linspace(0,1,M),repmat(d3+du,M,1),upper,'magenta','LineWidth',3)
-        set(gca,'FontSize',18);
-        ti = get(gca,'TightInset');
-        set(gca,'Position',[ti(1) ti(2) 1-ti(3)-ti(1) 1-ti(4)-ti(2)]);
-end
-
 upper_dis = sqrt(trapz(t,(upper_q - q_median).^2));
 lower_dis = sqrt(trapz(t,(lower_q - q_median).^2));
 whisker_dis = max([lower_dis upper_dis]);
@@ -115,19 +82,20 @@ outlier_index = [];
 for i = 1:N
     if dy(dy_ordering(N+1-i)) > whisker_dis
         outlier_index = [outlier_index; dy_ordering(N+1-i)];
-    else break
+    else
+        break
     end
 end
 
 % identify amplitude extremes
-distance_to_upper=repmat(inf,1,N);
-distance_to_lower=repmat(inf,1,N);
-out_50_CR = setdiff(setdiff([1:N], CR_50), outlier_index);
+distance_to_upper=inf(1,N);
+distance_to_lower=inf(1,N);
+out_50_CR = setdiff(setdiff((1:N), CR_50), outlier_index);
 for i = 1:length(out_50_CR)
     j = out_50_CR(i);
-    distance_to_upper(j) = sqrt(trapz(t,(upper_q - q_tilde(:,j)).^2)); 
-    distance_to_lower(j) = sqrt(trapz(t,(lower_q - q_tilde(:,j)).^2)); 
-end;
+    distance_to_upper(j) = sqrt(trapz(t,(upper_q - q_tilde(:,j)).^2));
+    distance_to_lower(j) = sqrt(trapz(t,(lower_q - q_tilde(:,j)).^2));
+end
 [~, max_index] = min(distance_to_upper);
 [~, min_index] = min(distance_to_lower);
 min_q = q_tilde(:,min_index);
@@ -135,105 +103,62 @@ max_q = q_tilde(:,max_index);
 minn = f_tilde(:,min_index);
 maxx = f_tilde(:,max_index);
 
-figure(310); clf; 
-    plot(t, f_median, 'black','linewidth', 2) 
+if (figs)
+    figure(310); clf;
+    plot(t, f_median, 'black','linewidth', 2);
     hold on;
-    plot(t, Q1, 'blue','linewidth', 2) 
-    plot(t, Q3,'green', 'linewidth', 2);
-    plot(t,minn,'red','linewidth',2);
-    plot(t,maxx,'magenta','linewidth',2);
+    plot(t, Q1, 'blue','linewidth', 2);
+    plot(t, Q3, 'blue', 'linewidth', 2);
+    plot(t, Q1a, 'green', 'linewidth', 2);
+    plot(t, Q3a, 'green', 'linewidth', 2);
+    plot(t,minn,'red', 'linewidth',2);
+    plot(t,maxx,'red', 'linewidth',2);
     xlim([t(1) t(end)]);
     ylim auto;
     set(gca,'FontSize',18);
     ti = get(gca,'TightInset');
     set(gca,'Position',[ti(1) ti(2) 1-ti(3)-ti(1) 1-ti(4)-ti(2)]);
 
-figure(311); clf; 
-    plot(t, minn,'red','linewidth',2);
-    xlim([t(1) t(end)]);
-    ylim auto;
-    set(gca,'FontSize',19)
-    ti = get(gca,'TightInset');
-    set(gca,'Position',[ti(1) ti(2) 1-ti(3)-ti(1) 1-ti(4)-ti(2)]);
-figure(312); clf; 
-    plot(t, Q1, 'blue','linewidth', 2);
-    xlim([t(1) t(end)]);
-    ylim auto;
-    set(gca,'FontSize',19)
-    ti = get(gca,'TightInset');
-    set(gca,'Position',[ti(1) ti(2) 1-ti(3)-ti(1) 1-ti(4)-ti(2)]);
-figure(313); clf; 
-    plot(t, f_median, 'black','linewidth', 2);
-    xlim([t(1) t(end)]);
-    ylim auto;
-    set(gca,'FontSize',19)
-    ti = get(gca,'TightInset');
-    set(gca,'Position',[ti(1) ti(2) 1-ti(3)-ti(1) 1-ti(4)-ti(2)]);
-figure(314); clf; 
-    plot(t, Q3,'green', 'linewidth', 2);
-    xlim([t(1) t(end)]);
-    ylim auto;
-    set(gca,'FontSize',19)
-    ti = get(gca,'TightInset');
-    set(gca,'Position',[ti(1) ti(2) 1-ti(3)-ti(1) 1-ti(4)-ti(2)]);
-figure(315); clf; 
-    plot(t, maxx,'magenta','linewidth',2);
-    xlim([t(1) t(end)]);
-    ylim auto;
-    set(gca,'FontSize',19)
-    ti = get(gca,'TightInset');
-    set(gca,'Position',[ti(1) ti(2) 1-ti(3)-ti(1) 1-ti(4)-ti(2)]);
-
-s = linspace(0,1,100);
-Fs2(:,1) = (1-s(1)) * minn + s(1) * Q1;    % Final surface plot
-for j=2:100
-    Fs2(:,j) = (1-s(j)) * minn + s(j) * Q1; 
-    Fs2(:,99+j) = (1-s(j)) * Q1 + s(j) * f_median; 
-    Fs2(:,198+j) = (1-s(j)) * f_median + s(j) * Q3; 
-    Fs2(:,297+j) = (1-s(j)) * Q3 + s(j) * maxx; 
-end
-d1=sqrt(trapz(t,(q_median-Q1_q).^2));
-dl=sqrt(trapz(t,(Q1_q-min_q).^2));
-d3=sqrt(trapz(t,(q_median-Q3_q).^2));
-du=sqrt(trapz(t,(Q3_q-max_q).^2));
-dmax = max(dl+d1, d3+du);
-dlp = (dl+d1) / dmax;
-d1p = d1 / dmax;
-d3p = d3 / dmax;
-dup = (d3+du) / dmax;
-part1=linspace(-d1-dl,-d1,100);
-part2=linspace(-d1,0,100);
-part3=linspace(0,d3,100);
-part4=linspace(d3,d3+du,100);
-allparts=[part1,part2(2:100),part3(2:100),part4(2:100)];
-[U,V]=meshgrid(t,allparts);
-U=U';
-V=V';
-figure(316); clf;
+    s = linspace(0,1,100);
+    Fs2 = zeros(length(t), 595);
+    Fs2(:,1) = (1-s(1)) * minn + s(1) * Q1;    % Final surface plot
+    for j=2:100
+        Fs2(:,j) = (1-s(j)) * minn + s(j) * Q1a;
+        Fs2(:,99+j) = (1-s(j)) * Q1a + s(j) * Q1;
+        Fs2(:,198+j) = (1-s(j)) * Q1 + s(j) * f_median;
+        Fs2(:,297+j) = (1-s(j)) * f_median + s(j) * Q3;
+        Fs2(:,396+j) = (1-s(j)) * Q3 + s(j) * Q3a;
+        Fs2(:,495+j) = (1-s(j)) * Q3a + s(j) * maxx;
+    end
+    d1=sqrt(trapz(t,(q_median-Q1_q).^2));
+    d1a=sqrt(trapz(t,(Q1_q-Q1a_q).^2));
+    dl=sqrt(trapz(t,(Q1a_q-min_q).^2));
+    d3=sqrt(trapz(t,(q_median-Q3_q).^2));
+    d3a=sqrt(trapz(t,(Q3_q-Q3a_q).^2));
+    du=sqrt(trapz(t,(Q3a_q-max_q).^2));
+    part1=linspace(-d1-d1a-dl,-d1-d1a,100);
+    part2=linspace(-d1-d1a,-d1,100);
+    part3=linspace(-d1,0,100);
+    part4=linspace(0,d3,100);
+    part5=linspace(d3,d3+d3a,100);
+    part6=linspace(d3+d3a,d3+d3a+du,100);
+    allparts=[part1,part2(2:100),part3(2:100),part4(2:100),part5(2:100),part6(2:100)];
+    [U,V]=meshgrid(t,allparts);
+    U=U';
+    V=V';
+    figure(311); clf;
     surf(U,V,Fs2);
     hold on;
     shading flat;
     light;
     plot3(t,zeros(1,M),f_median,'k','LineWidth',3)
     plot3(t,repmat(-d1,M,1),Q1,'b','LineWidth',3)
-    plot3(t,repmat(-d1-dl,M,1),minn,'r','LineWidth',3)
-    plot3(t,repmat(d3,M,1),Q3,'g','LineWidth',3)
-    plot3(t,repmat(d3+du,M,1),maxx,'magenta','LineWidth',3)
+    plot3(t,repmat(-d1-d1a,M,1),Q1a,'g','LineWidth',3)
+    plot3(t,repmat(-d1-d1a-dl,M,1),minn,'r','LineWidth',3)
+    plot3(t,repmat(d3,M,1),Q3,'b','LineWidth',3)
+    plot3(t,repmat(d3+d3a,M,1),Q3a,'g','LineWidth',3)
+    plot3(t,repmat(d3+d3a+du,M,1),maxx,'r','LineWidth',3)
     set(gca,'FontSize',18);
     ti = get(gca,'TightInset');
     set(gca,'Position',[ti(1) ti(2) 1-ti(3)-ti(1) 1-ti(4)-ti(2)]);
-    
-for i = 1:length(outlier_index)   % display amplitude outliers
-    figure(320 + i); clf;
-        for j = 1:N
-            plot(t,f_tilde(:,j),'green','linewidth',2);
-            hold on;
-        end
-        plot(t,f_median,'black','linewidth',2);
-        plot(t,f_tilde(:,outlier_index(i)),'red','linewidth',2);
-        xlim([t(1) t(end)]);
-        ylim auto;
-        set(gca,'FontSize',19)
-        ti = get(gca,'TightInset');
-        set(gca,'Position',[ti(1) ti(2) 1-ti(3)-ti(1) 1-ti(4)-ti(2)]);
 end
