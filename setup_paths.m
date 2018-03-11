2da374d8b400b59c6652a8a9f381722ec5d31a8b
Derek Tucker <jdtuck@sandia.gov>
2018-03-10 18:49:04 -0700

    update paths

--- setup_paths.m
+++ setup_paths.m
@@ -1,4 +1,8 @@
 addpath(genpath('minFunc'))
 addpath(genpath('DP'))
+addpath(genpath('bayesian'))
+addpath(genpath('bspline_tools'))
+addpath(genpath('basis'))
+addpath(genpath('data'))
 addpath(genpath('gropt'))
 addpath(genpath('mlogit_warp'))
