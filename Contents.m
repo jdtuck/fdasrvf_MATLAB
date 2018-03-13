35077651ab5d6f669dc986302b046cb643034021
J. Derek Tucker <jdtuck@sandia.gov>
2018-03-13 08:43:02 -0600

    add elastic distance function

--- Contents.m
+++ Contents.m
@@ -1,27 +1,24 @@
-% FDASRVF
+% FDASRVF_MATLAB
 %
 % Files
 %   Amplitude_Boxplot        - Functional Amplitude Boxplot
-
 %   bootstrap_tol            - Boostrapped tolerance bounds
 %   create_basismatrix       - Create Basis Matrix
 %   cumsimps                 - Cumulative Simpson's numerical integration.
 %   cumtrapzmid              - Cummulative Trapezodial Integration from mid point
+%   elastic_distance         - Calculates two elastic distances
 %   elastic_logistic         - Elastic Logistic Functional Regression
 %   elastic_mlogistic        - Elastic Multinomial Logistic Functional Regression
 %   elastic_prediction       - Elastic Functional Regression Prediction
 %   elastic_regression       - Elastic Functional Regression
 %   exp_map                  - Exponential Map
-
 %   f_to_srvf                - Convert function to Square-Root Velocity Function
 %   gauss_model              - Gaussian gnerative model
 %   horizFPCA                - Horizontal Functional Principal Component Analysis
 %   inner_product            - Functional inner product
-
 %   inv_exp_map              - Inverse Exponential Map
 %   invertGamma              - Invert Warping Function
 %   L2norm                   - L2 Functional Norm
-
 %   multiple_align_functions - Group-wise function alignment to specified mean
 %   optimum_reparam          - Calculates Warping for two SRVFs
 %   outlier_detection        - Outlier Detection
@@ -30,15 +27,12 @@
 %   Phase_Boxplot            - Functional phase Boxplot
 %   progress                 - Wrapper class to provide an iterator object for loop creation
 %   ProgressBar              - A class to provide a convenient and useful progress bar
-
 %   rgam                     - Random Warping
 %   setup                    - setup environment
 %   setup_paths              - setup paths
 %   simps                    - Simpson's numerical integration.
 %   simul_align              - Align two functions by simultaneous reparameterization
 %   simul_gam                - Align two function using simultaneous alignments.
-
-
 %   smooth_data              - Smooth Functions
 %   SqrtMean                 - SRVF transform of warping functions
 %   SqrtMeanInverse          - SRVF transform of warping functions
@@ -51,12 +45,3 @@
 %   vertFPCA                 - Vertical Functional Principal Component Analysis
 %   warp_f_gamma             - Warp Function
 %   warp_q_gamma             - WARP_F_GAMMA Warp SRVF
-
-
-
-
-
-
-%   outlier_detection        - Outlier Detection
-
-
---
+++ elastic_distance.m
@@ -0,0 +1,33 @@
+function [dy, dx] = elastic_distance(f1, f2, time, lambda)
+% ELASTIC_DISTANCE Calculates two elastic distances
+% -------------------------------------------------------------------------
+% This functions calculates the distances between functions,
+% \eqn{D_y} and \eqn{D_x}, where function 1 is aligned to function 2
+%
+% Usage: [dy, dx] = elastic_distance(f1, f2, time)
+%        [dy, dx] = elastic_distance(f1, f2, time, lambda)
+%
+% Input:
+% f1: sample function 1
+% f2: sample function 1
+% time: vector of size \eqn{N} describing the sample points
+% lambda controls amount of warping (default = 0)
+%
+% Output
+% dy: amplitude distance
+% dx: phase distance
+if nargin < 4
+  lambda = 0;
+end
+q1 = f_to_srvf(f1,time);
+q2 = f_to_srvf(f2,time);
+gam = optimum_reparam(q1,q2,time,lambda);
+fw = warp_f_gamma(f2,gam,time);
+qw = f_to_srvf(fw,time);
+dy = sqrt(trapz(time,(q1-qw).^2));
+
+time1 = linspace(0,1,length(time));
+binsize = mean(diff(time1));
+psi = sqrt(gradient(gam,binszie));
+v = inv_exp_map(ones(1,length(gam)), psi);
+dx = sqrt(trapz(time,v.^2));
