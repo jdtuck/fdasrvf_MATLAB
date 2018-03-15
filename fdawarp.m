f243ce63c5e03f601678536c2ce6379725f078c5
Derek Tucker <jdtuck@sandia.gov>
2018-03-14 20:24:31 -0600

    added median time_warping

--- fdawarp.m
+++ fdawarp.m
@@ -257,6 +257,251 @@ classdef fdawarp
             obj.rsamps = false;
         end
         
+        function obj = time_warping_median(obj,lambda,option)
+            % TIME_WARPING_MEDIAN Group-wise function alignment to median
+            % -------------------------------------------------------------------------
+            % This function aligns a collection of functions using the elastic square-root
+            % slope (srsf) framework to the median
+            %
+            % Input:
+            % f (M,N): matrix defining N functions of M samples
+            % t : time vector of length M
+            % lambda: regularization parameter
+            %
+            % default options
+            % option.parallel = 0; % turns offs MATLAB parallel processing (need
+            % parallel processing toolbox)
+            % option.closepool = 0; % determines wether to close matlabpool
+            % option.smooth = 0; % smooth data using standard box filter
+            % option.sparam = 25; % number of times to run filter
+            % option.showplot = 1; % turns on and off plotting
+            % option.method = 'DP'; % optimization method (DP, DP2, SIMUL, RBFGS)
+            % option.w = 0.0; % BFGS weight
+            % option.MaxItr = 20;  % maximum iterations
+            %
+            % Output:
+            % fdawarp object
+            if nargin < 2
+                lambda = 0;
+                option.parallel = 0;
+                option.closepool = 0;
+                option.smooth = 0;
+                option.sparam = 25;
+                option.showplot = 1;
+                option.method = 'DP1';
+                option.w = 0.0;
+                option.MaxItr = 20;
+            elseif nargin < 3
+                option.parallel = 0;
+                option.closepool = 0;
+                option.smooth = 0;
+                option.sparam = 25;
+                option.showplot = 1;
+                option.method = 'DP1';
+                option.w = 0.0;
+                option.MaxItr = 20;
+            end
+            % time warping on a set of functions
+            if option.parallel == 1
+                if isempty(gcp('nocreate'))
+                    % prompt user for number threads to use
+                    nThreads = input('Enter number of threads to use: ');
+                    if nThreads > 1
+                        parpool(nThreads);
+                    elseif nThreads > 12 % check if the maximum allowable number of threads is exceeded
+                        while (nThreads > 12) % wait until user figures it out
+                            fprintf('Maximum number of threads allowed is 12\n Enter a number between 1 and 12\n');
+                            nThreads = input('Enter number of threads to use: ');
+                        end
+                        if nThreads > 1
+                            parpool(nThreads);
+                        end
+                    end
+                end
+            end
+            %% Parameters
+            
+            fprintf('\n lambda = %5.1f \n', lambda);
+            
+            t = obj.time;
+            
+            binsize = mean(diff(t));
+            [M, N] = size(obj.f);
+            f1 = obj.f;
+            
+            if option.smooth == 1
+                f1 = smooth_data(f1, option.sparam);
+            end
+            
+            if option.showplot == 1
+                figure(1); clf;
+                plot(t, obj.f, 'linewidth', 1);
+                title('Original data', 'fontsize', 16);
+                pause(0.1);
+            end
+            
+            %% Compute the q-function of the plot
+            q = f_to_srvf(obj.f,t);
+            
+            %% Set initial using the original f space
+            fprintf('\nInitializing...\n');
+            mnq = mean(q,2);
+            dqq = sqrt(sum((q - mnq*ones(1,N)).^2,1));
+            [~, min_ind] = min(dqq);
+            mq = q(:,min_ind);
+            mf = f1(:,min_ind);
+            
+            gam_o = zeros(N,size(q,1));
+            if option.parallel == 1
+                parfor k = 1:N
+                    q_c = q(:,k,1); mq_c = mq;
+                    gam_o(k,:) = optimum_reparam(mq_c,q_c,t,lambda,option.method,option.w, ...
+                        mf(1), f1(1,k,1));
+                end
+            else
+                for k = 1:N
+                    q_c = q(:,k,1); mq_c = mq;
+                    gam_o(k,:) = optimum_reparam(mq_c,q_c,t,lambda,option.method,option.w, ...
+                        mf(1), f1(1,k,1));
+                end
+            end
+            
+            gamI_o = SqrtMeanInverse(gam_o);
+            mf = warp_f_gamma(mf,gamI_o,t);
+            mq = f_to_srvf(mf,t);
+            
+            %% Compute Mean
+            fprintf('Computing Karcher median of %d functions in SRVF space...\n',N);
+            ds = inf;
+            MaxItr = option.MaxItr;
+            qun_o = zeros(1,MaxItr);
+            f_temp = zeros(length(t),N);
+            q_temp = zeros(length(t),N);
+            for r = 1:MaxItr
+                fprintf('updating step: r=%d\n', r);
+                if r == MaxItr
+                    fprintf('maximal number of iterations is reached. \n');
+                end
+                
+                % Matching Step
+                clear gam gam_dev;
+                % use DP to find the optimal warping for each function w.r.t. the mean
+                gam_o = zeros(N,size(q,1));
+                gam_dev = zeros(N,size(q,1));
+                vtil = zeros(M,N);
+                dtil = zeros(1,N);
+                if option.parallel == 1
+                    parfor k = 1:N
+                        q_c = q(:,k,1); mq_c = mq(:,r);
+                        gam_o(k,:) = optimum_reparam(mq_c,q_c,t,lambda,option.method,option.w, ...
+                            mf(1,r), f1(1,k,1));
+                        gam_dev(k,:) = gradient(gam_o(k,:), 1/(M-1));
+                        f_temp(:,k) = warp_f_gamma(f1(:,k,1),gam_o(k,:),t);
+                        q_temp(:,k) = f_to_srvf(f_temp(:,k),t);
+                        v = q_temp(:,k) - mq_c
+                        d = sqrt(trapz(t, v.*v));
+                        vtil(:,k) = v/d;
+                        dtil(k) = 1/d;
+                    end
+                else
+                    for k = 1:N
+                        q_c = q(:,k,1); mq_c = mq(:,r);
+                        gam_o(k,:) = optimum_reparam(mq_c,q_c,t,lambda,option.method,option.w, ...
+                            mf(1,r), f1(1,k,1));
+                        gam_dev(k,:) = gradient(gam_o(k,:), 1/(M-1));
+                        f_temp(:,k) = warp_f_gamma(f1(:,k,1),gam_o(k,:),t);
+                        q_temp(:,k) = f_to_srvf(f_temp(:,k),t);
+                        v = q_temp(:,k) - mq_c;
+                        d = sqrt(trapz(t, v.*v));
+                        vtil(:,k) = v/d;
+                        dtil(k) = 1/d;
+                    end
+                end
+                q(:,:,r+1) = q_temp;
+                f1(:,:,r+1) = f_temp;
+                
+                ds_tmp = sqrt(sum(simps(t,(q(:,:,r+1)-mq(:,r)*ones(1,N)).^2)))+lambda*sum(simps(t,(1-sqrt(gam_dev')).^2));
+                if (isreal(ds_tmp))
+                    ds(r+1) = ds_tmp;
+                else
+                    ds(r+1) = abs(ds_tmp);
+                end
+                
+                
+                % Minimization Step
+                % compute the mean of the matched function
+                stp = .3;
+                vbar = sum(vtil,2)*sum(dtil)^(-1);
+                mq(:,r+1) = mq(:,r) + stp*vbar;
+                mf(:,r+1) = median(f1(1,:,1)) + cumtrapz(t, mq(:,r+1).*abs(mq(:,r+1)));
+                
+                qun_o(r) = norm(mq(:,r+1)-mq(:,r))/norm(mq(:,r));
+                if qun_o(r) < 1e-2 || r >= MaxItr
+                    break;
+                end
+            end
+            
+            % last step with centering of gam
+            r = r+1;
+            if option.parallel == 1
+                parfor k = 1:N
+                    q_c = q(:,k,1); mq_c = mq(:,r);
+                    gam_o(k,:) = optimum_reparam(mq_c,q_c,t,lambda,option.method,option.w, ...
+                        mf(1,r), f1(1,k,1));
+                end
+            else
+                for k = 1:N
+                    q_c = q(:,k,1); mq_c = mq(:,r);
+                    gam_o(k,:) = optimum_reparam(mq_c,q_c,t,lambda,option.method,option.w, ...
+                        mf(1,r), f1(1,k,1));
+                end
+            end
+            
+            gamI_o = SqrtMeanInverse(gam_o);
+            mq(:,r+1) = warp_q_gamma(mq(:,r),gamI_o,t);
+            for k = 1:N
+                q(:,k,r+1) = warp_q_gamma(q(:,k,r),gamI_o,t);
+                f1(:,k,r+1) = warp_q_gamma(f1(:,k,r),gamI_o,t);
+                gamI_o(k,:) = interp1(t, gamI_o(k,:), (t(end)-t(1)).*gamI_o + t(1));
+            end
+            
+            %% Aligned data & stats
+            obj.fn = f1(:,:,r+1);
+            obj.qn = q(:,:,r+1);
+            obj.q0 = q(:,:,1);
+            std_f0 = std(f0, 0, 2);
+            std_fn = std(fn, 0, 2);
+            obj.mqn = mq(:,r+1);
+            fmedian = mean(obj.f(1,:))+cumtrapz(t,obj.mqn.*abs(obj.mqn));
+            
+            fgam = zeros(M,N);
+            for ii = 1:N
+                fgam(:,ii) = warp_f_gamma(fmedian,gam_o(ii,:),t);
+            end
+            var_fgam = var(fgam,[],2);
+            
+            obj.stats.orig_var = trapz(t,std_f0.^2);
+            obj.stats.amp_var = trapz(t,std_fn.^2);
+            obj.stats.phase_var = trapz(t,var_fgam);
+            
+            obj.gam = gam_o.';
+            [~,fy] = gradient(obj.gam,binsize,binsize);
+            obj.psi = sqrt(fy+eps);
+            
+            if option.parallel == 1 && option.closepool == 1
+                if isempty(gcp('nocreate'))
+                    delete(gcp('nocreate'))
+                end
+            end
+            
+            
+            obj.qun = qun_o(1:r-1);
+            obj.lambda = lambda;
+            obj.method = option.method;
+            obj.gamI = gamI_o;
+            obj.rsamps = false;
+        end
+        
         function plot(obj)
             % plot plot functional alignment results
             % -------------------------------------------------------------------------
