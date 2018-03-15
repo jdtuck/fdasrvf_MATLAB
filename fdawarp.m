e2f3299d6dddc75b95e42479e1cb898dc402f0b5
J. Derek Tucker <jdtuck@sandia.gov>
2018-03-15 11:51:11 -0600

    added gauss_model

--- fdawarp.m
+++ fdawarp.m
@@ -24,7 +24,7 @@ classdef fdawarp
         ft     % random warped functions
         qs     % random aligned srvfs
         type   % alignment type
-        
+
     end
     
     methods
@@ -508,6 +508,111 @@ classdef fdawarp
             obj.gamI = gamI_o;
             obj.rsamps = false;
             obj.type = 'median';
+        end
+        
+        function obj = gauss_model(obj, n, sort_samples)
+            % GAUSS_MODEL Gaussian gnerative model
+            % -------------------------------------------------------------------------
+            % This function models the functional data using a Gaussian model extracted
+            % from the principal components of the srvfs
+            %
+            % Usage: samples = gauss_model(obj, n, sort_samples)
+            %
+            % Inputs:
+            % n: number of random samples (n = 1)
+            % sort_samples: sort samples (e.g., = F)
+            %
+            % Output:
+            % fdawarp object
+          
+            
+            if nargin < 2
+                n = 1;
+                sort_samples = false;
+            elseif nargin < 3
+                sort_samples = false;
+            end
+            %% Separated and Warped Data
+            % sampling from the estimated model
+            [M, ~] = size(obj.fn);
+            binsize = mean(diff(obj.time));
+            
+            % compute mean and covariance in q-domain
+            id = round(length(obj.time)/2);
+            q_new = obj.qn;
+            mq_new = mean(obj.qn,2);
+            m_new = sign(obj.fn(id,:)).*sqrt(abs(obj.fn(id,:)));
+            mqn2 = [mq_new; mean(m_new)];
+            C = cov([q_new;m_new]');
+            
+            q_s = mvnrnd(mqn2', C, n)';
+            
+            % compute the correspondence to the original function domain
+            f_s = zeros(M,n);
+            for k = 1:n
+                f_s(:,k) = cumtrapzmid(obj.time,q_s(1:(end-1),k).*abs(q_s(1:(end-1),k)),sign(q_s(end,k))*(q_s(end,k)^2),id);
+            end
+            fbar = mean(obj.fn,2);
+            fsbar = mean(f_s,2);
+            err = repmat(fbar-fsbar,1,n);
+            f_s = f_s + err;
+            
+            % random warping generation
+            rgam = randomGamma(obj.gam.',n);
+            
+            % combine samples
+            if sort_samples
+                %%%% sort functions and warpings
+                mx = max(f_s);
+                [~, seq1] = sort(mx);
+                
+                % compute the psi-function
+                psi1 = zeros(n,size(rgam,2));
+                len = zeros(1,n);
+                ip = zeors(1,n);
+                for i = 1:n
+                    psi1(i,:) = gradient(rgam(i,:), binsize)./sqrt(abs(gradient(rgam(i,:), binsize))+eps);
+                    ip(i) = ones(1,M)*psi1(i,:)'/M;
+                    len(i) = acos(ones(1,M)*psi1(i,:)'/M);
+                end
+                [~, seq2] = sort(len);
+                
+                % combine x-variability and y-variability
+                f_c = zeros(size(obj.fn,1),n);
+                for k = 1:n
+                    f_c(:,k) = interp1((0:M-1)/(M-1), f_s(:,seq1(k)), invertGamma(rgam(seq2(k),:)')');
+                    while sum(isnan(f_c(:,k))) >= 1
+                        rgam2 = randomGamma(obj.gam.',1);
+                        f_c(:,k) = warp_f_gamma(f_s(:,k), invertGamma(rgam2.'), (0:M-1)/(M-1));
+                    end
+                end
+            else
+                % compute the psi-function
+                psi1 = zeros(n,size(rgam,2));
+                len = zeros(1,n);
+                ip = zeros(1,n);
+                for i = 1:n
+                    psi1(i,:) = gradient(rgam(i,:), binsize)./sqrt(abs(gradient(rgam(i,:), binsize))+eps);
+                    ip(i) = ones(1,M)*psi1(i,:)'/M;
+                    len(i) = acos(ones(1,M)*psi1(i,:)'/M);
+                end
+                
+                % combine x-variability and y-variability
+                f_c = zeros(size(obj.fn,1),n);
+                for k = 1:n
+                    f_c(:,k) = interp1((0:M-1)/(M-1), f_s(:,k), invertGamma(rgam(k,:)')');
+                    while sum(isnan(f_c(:,k))) >= 1
+                        rgam2 = randomGamma(obj.gam.',1);
+                        f_c(:,k) = warp_f_gamma(f_s(:,k), invertGamma(rgam2.'), (0:M-1)/(M-1));
+                    end
+                end
+            end
+            
+            obj.fs = f_s;
+            obj.gams = rgam;
+            obj.ft = f_c;
+            obj.qs = q_s(1:M,:);
+            obj.rsamps = true;
         end
         
         function plot(obj)
