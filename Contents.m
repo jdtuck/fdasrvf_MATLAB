6d622fc87be6eba80e04698b6cec18d625f72a56
Derek Tucker <jdtuck@sandia.gov>
2018-03-17 17:59:14 -0600

    update contents

--- Contents.m
+++ Contents.m
@@ -1,55 +1,46 @@
 % FDASRVF
 %
 % Files
-%   align_fPCA               - Group-wise function alignment and PCA Extractions
-%   ampbox                   - ampbox A class to amplitude box plots
-%   Amplitude_Boxplot        - Functional Amplitude Boxplot
-%   bootstrap_tol            - Boostrapped tolerance bounds
-%   cumsimps                 - Cumulative Simpson's numerical integration.
-%   cumtrapzmid              - Cummulative Trapezodial Integration from mid point
-%   elastic_distance         - Calculates two elastic distances
-%   elastic_logistic         - Elastic Logistic Functional Regression
-%   elastic_mlogistic        - Elastic Multinomial Logistic Functional Regression
-%   elastic_prediction       - Elastic Functional Regression Prediction
-%   elastic_regression       - Elastic Functional Regression
-%   exp_map                  - Exponential Map
-%   f_to_srvf                - Convert function to Square-Root Velocity Function
-%   fdahpca                  - fdahpca A class to provide a horizontal fPCA
-%   fdakma                   - fdakma A class to provide a kmeans clustering and alignment
-%   fdavpca                  - fdavpca A class to provide a vertical fPCA
-%   fdawarp                  - fdawarp A class to provide a SRVF functional data analysis
-%   gauss_model              - Gaussian gnerative model
-%   horizFPCA                - Horizontal Functional Principal Component Analysis
-%   inner_product            - Functional inner product
-%   inv_exp_map              - Inverse Exponential Map
-%   invertGamma              - Invert Warping Function
-%   kmeans                   - K-Means clustering and alignment
-%   L2norm                   - L2 Functional Norm
-%   multiple_align_functions - Group-wise function alignment to specified mean
-%   optimum_reparam          - Calculates Warping for two SRVFs
-%   outlier_detection        - Outlier Detection
-%   pairwise_align           - Align two functions
-%   pairwise_align_bayes     - PAIRWISE_ALIGN Align two functions using Bayesian method
-%   Phase_Boxplot            - Functional phase Boxplot
-%   phbox                    - phbox A class to phase box plots
-%   progress                 - Wrapper class to provide an iterator object for loop creation
-%   ProgressBar              - A class to provide a convenient and useful progress bar
-%   rgam                     - Random Warping
-%   setup                    - setup environment
-%   setup_paths              - setup paths
-%   simps                    - Simpson's numerical integration.
-%   simul_align              - Align two functions by simultaneous reparameterization
-%   simul_gam                - Align two function using simultaneous alignments.
-%   smooth_data              - Smooth Functions
-%   SqrtMean                 - SRVF transform of warping functions
-%   SqrtMeanInverse          - SRVF transform of warping functions
-%   SqrtMedian               - SRVF transform of warping functions
-%   srvf_to_f                - Convert SRSF to f
-%   time_warping             - Group-wise function alignment
-%   time_warping_median      - Group-wise function alignment to median
-%   Translation_Boxplot      - Translation Boxplot
-%   updateParallel           - Update function when ProgressBar is used in parallel setup
-%   varycolor                - Produces colors with maximum variation on plots with multiple
-%   vertFPCA                 - Vertical Functional Principal Component Analysis
-%   warp_f_gamma             - Warp Function
-%   warp_q_gamma             - WARP_F_GAMMA Warp SRVF
+%   align_fPCA           - Group-wise function alignment and PCA Extractions
+%   ampbox               - ampbox A class to amplitude box plots
+%   bootstrap_tol        - Boostrapped tolerance bounds
+%   create_basismatrix   - Create Basis Matrix
+%   cumsimps             - Cumulative Simpson's numerical integration.
+%   cumtrapzmid          - Cummulative Trapezodial Integration from mid point
+%   elastic_distance     - Calculates two elastic distances
+%   elastic_logistic     - elastic_logistic A class to provide a SRVF logistic
+%   elastic_mlogistic    - elastic_mlogistic A class to provide a SRVF multionomial logistic
+%   elastic_regression   - elastic_regression A class to provide a SRVF regression
+%   exp_map              - Exponential Map
+%   f_to_srvf            - Convert function to Square-Root Velocity Function
+%   fdahpca              - fdahpca A class to provide a horizontal fPCA
+%   fdakma               - fdakma A class to provide a kmeans clustering and alignment
+%   fdavpca              - fdavpca A class to provide a vertical fPCA
+%   fdawarp              - fdawarp A class to provide a SRVF functional data analysis
+%   inner_product        - Functional inner product
+%   inv_exp_map          - Inverse Exponential Map
+%   invertGamma          - Invert Warping Function
+%   L2norm               - L2 Functional Norm
+%   optimum_reparam      - Calculates Warping for two SRVFs
+%   outlier_detection    - Outlier Detection
+%   pairwise_align       - Align two functions
+%   pairwise_align_bayes - PAIRWISE_ALIGN Align two functions using Bayesian method
+%   phbox                - phbox A class to phase box plots
+%   progress             - Wrapper class to provide an iterator object for loop creation
+%   ProgressBar          - A class to provide a convenient and useful progress bar
+%   rgam                 - Random Warping
+%   setup                - setup environment
+%   setup_paths          - setup paths
+%   simps                - Simpson's numerical integration.
+%   simul_align          - Align two functions by simultaneous reparameterization
+%   simul_gam            - Align two function using simultaneous alignments.
+%   smooth_data          - Smooth Functions
+%   SqrtMean             - SRVF transform of warping functions
+%   SqrtMeanInverse      - SRVF transform of warping functions
+%   SqrtMedian           - SRVF transform of warping functions
+%   srvf_to_f            - Convert SRSF to f
+%   Translation_Boxplot  - Translation Boxplot
+%   updateParallel       - Update function when ProgressBar is used in parallel setup
+%   varycolor            - Produces colors with maximum variation on plots with multiple
+%   warp_f_gamma         - Warp Function
+%   warp_q_gamma         - WARP_F_GAMMA Warp SRVF
