30c96495c7c883c5ac523498a110b159c79c1c11
Derek Tucker <jdtuck@sandia.gov>
2018-03-24 06:27:31 -0600

    started time_warping_bayes function

--- time_warping_bayes.m
+++ time_warping_bayes.m
@@ -1,8 +1,9 @@
-function out = pairwise_align_bayes(f1i, f2i, time, mcmcopts)
-% PAIRWISE_ALIGN Align two functions using Bayesian method
+function out = time_warping_bayes(f, time, mcmcopts)
+% TIME_WARPING_BAYES Align multiple functions using Bayesian method
 % -------------------------------------------------------------------------
-% This function aligns two functions using Bayesian framework. It will align
-% f2 to f1. It is based on mapping warping functions to a hypersphere, and a
+% This function aligns a collection of functions using the elastic square-root
+% slope (srsf) framework. This method uses a Bayesian approach.
+% It is based on mapping warping functions to a hypersphere, and a
 % subsequent exponential mapping to a tangent space. In the tangent space,
 % the Z-mixture pCN algorithm is used to explore both local and global
 % structure in the posterior distribution.
@@ -13,12 +14,11 @@ function out = pairwise_align_bayes(f1i, f2i, time, mcmcopts)
 % larger coefficients corresponding to larger shifts in parameter space. The
 % zpcn$probs give the probability of each shift size.
 %
-% Usage:  out = pairwise_align_bayes(f1i, f2i, time)
-%         out = pairwise_align_bayes(f1i, f2i, time, mcmcopts)
+% Usage:  out = time_warping_bayes(f, time)
+%         out = time_warping_bayes(f, time, mcmcopts)
 %
 % Input:
-% f1i: vector defining M samples of function 1
-% f2i: vector defining M samples of function 2
+% f: (M,N): matrix defining N functions of M samples
 % time: time vector of length M
 %
 % default mcmc options
@@ -30,16 +30,16 @@ function out = pairwise_align_bayes(f1i, f2i, time, mcmcopts)
 % tmp.probs = [0.1,0.1,0.7,0.1];
 % mcmcopts.zpcn = tmp;
 % mcmcopts.propvar = 1; % proposal vairance
-% mcmcopts.initcoef = repelem(0, 20).'; % init coef
+% mcmcopts.initcoef = zeros(20, size(f,2)) % init coef
 % mcmcopts.npoints = 200; % number of sample interpolation points
 % mcmcopts.extrainfo = true; % return extra info about mcmc
 %
 % Output:
 % structure containing
-% out.f2_warped: aligned f2
-% out.gamma: warping function
-% out.g_coef: final g_coef
-% out.psi: final psi
+% out.f_warped: aligned f
+% out.gamma: warping functions
+% out.g_coef: final g_coefs
+% out.psi: final psis
 % out.sigma1: final sigma
 %
 % if extrainfo
@@ -50,9 +50,10 @@ function out = pairwise_align_bayes(f1i, f2i, time, mcmcopts)
 % out.gamma_stats: posterior gamma stats
 % out.xdist: phase distance posterior
 % out.ydist: amplitude distance posterior
+N = size(f,2);
 
 if nargin < 4
-    mcmcopts.iter = 2e4;
+    mcmcopts.iter = 200;%2e4;
     mcmcopts.burnin = min(5e3,mcmcopts.iter/2);
     mcmcopts.alpha0 = 0.1;
     mcmcopts.beta0 = 0.1;
@@ -60,30 +61,23 @@ if nargin < 4
     tmp.probs = [0.1,0.1,0.7,0.1];
     mcmcopts.zpcn = tmp;
     mcmcopts.propvar = 1;
-    mcmcopts.initcoef = repelem(0, 20).';
+    mcmcopts.initcoef = zeros(20, N);
     mcmcopts.npoints = 200;
     mcmcopts.extrainfo = true;
 end
 
-if (~iscolumn(f1i))
-    f1i = f1i.';
-end
-if (~iscolumn(f2i))
-    f2i = f2i.';
-end
 if (~iscolumn(time))
     time = time.';
 end
-if (length(f1i) ~= length(f2i))
-    error('Length of f1 and f2 must be equal')
-end
-if (length(f1i) ~= length(time))
+
+if (size(f,1) ~= length(time))
     error('Length of f1 and time must be equal')
 end
+
 if (length(mcmcopts.zpcn.betas) ~= length(mcmcopts.zpcn.probs))
     error('In zpcn, betas must equal length of probs')
 end
-if (mod(length(mcmcopts.initcoef), 2) ~= 0)
+if (mod(size(mcmcopts.initcoef), 1) ~= 0)
     error('Length of mcmcopts.initcoef must be even')
 end
 
@@ -91,28 +85,23 @@ end
 SIG_GAM = 13;
 iter = mcmcopts.iter;
 
-% for now, back to struct format of Yi's software
-f1.x = time;
-f1.y = f1i;
-f2.x = time;
-f2.y = f2i;
-
 % normalize timet to [0,1]
 % ([a,b] - a) / (b-a) = [0,1]
-f1.x = (f1.x-min(f1.x))./(max(f1.x)-min(f1.x));
-f2.x = (f2.x-min(f2.x))./(max(f2.x)-min(f2.x));
+time = (time-min(time))./(max(time)-min(time));
 
 % parameter settings
 pw_sim_global_burnin = mcmcopts.burnin;
 valid_index = pw_sim_global_burnin:iter;
-pw_sim_global_Mg = length(mcmcopts.initcoef)/2;
+pw_sim_global_Mg = size(mcmcopts.initcoef,1)/2;
 g_coef_ini = mcmcopts.initcoef;
 numSimPoints = mcmcopts.npoints;
 pw_sim_global_domain_par = linspace(0,1,numSimPoints).';
 g_basis = basis_fourier(pw_sim_global_domain_par, pw_sim_global_Mg, 1);
+pw_sim_global_Mq = length(g_basis.x)/2;
 sigma1_ini = 1;
 zpcn = mcmcopts.zpcn;
 pw_sim_global_sigma_g = mcmcopts.propvar;
+pw_sim_global_sigma_q = mcmcopts.propvar;
 
     function result = propose_g_coef(g_coef_curr)
         pCN_beta = zpcn.betas;
@@ -128,32 +117,63 @@ pw_sim_global_sigma_g = mcmcopts.propvar;
         end
     end
 
-% srsf transformation
-q1 = f_Q(f1);
-q2 = f_Q(f2);
+    function result = propose_q_star(q_star_curr)
+        pCN_beta = zpcn.betas;
+        pCN_prob = zpcn.probs;
+        probm = [0, cumsum(pCN_prob)];
+        z = rand;
+        for i = 1:length(pCN_beta)
+            if (z <= probm(i+1) && z > probm(i))
+                q_star_new = normrnd(0, pw_sim_global_sigma_q ./ repelem(1:pw_sim_global_Mq,2), 1, pw_sim_global_Mq * 2);
+                result.prop = sqrt(1-pCN_beta(i)^2) * q_star_curr + pCN_beta(i) * q_star_new.';
+                result.ind = i;
+            end
+        end
+    end
 
-tmp = f_exp1(f_basistofunction(g_basis.x,0,g_coef_ini,g_basis, false));
-if (min(tmp.y)<0)
-    error("Invalid initial value of g")
+% srsf transformation
+f0 = zeros(length(g_basis.x),N);
+for ii = 1:N
+    f0(:,ii) = interp1(time,f(:,ii),g_basis.x,'linear');
+end
+qo = f_to_srvf(f0, g_basis.x);
+
+
+for ii = 1:N
+    tmp = f_exp1(f_basistofunction(g_basis.x,0,g_coef_ini(:,ii),g_basis, false));
+    if (min(tmp.y)<0)
+        error("Invalid initial value of g for sample %d", ii)
+    end
 end
 
 % result objects
-result.g_coef = zeros(iter,length(g_coef_ini));
+result.g_coef = zeros(iter,size(g_coef_ini,1),size(g_coef_ini,2));
+result.q_star = zeros(iter,length(g_basis.x));
 result.sigma1 = zeros(1,iter);
 result.logl = zeros(1,iter);
-result.SSE = zeros(1,iter);
+result.SSE = zeros(iter,N);
 result.accept = zeros(1,iter);
 result.accept_betas = zeros(1,iter);
 
 % init
 g_coef_curr = g_coef_ini;
 sigma1_curr = sigma1_ini;
-SSE_curr = f_SSEg_pw(f_basistofunction(g_basis.x,0,g_coef_ini,g_basis,false),q1,q2);
-logl_curr = f_logl_pw(f_basistofunction(g_basis.x,0,g_coef_ini,g_basis,false),q1,q2,sigma1_ini^2,SSE_curr);
+mnq = mean(qo,2);
+dqq = sqrt(sum((qo - mnq*ones(1,N)).^2,1));
+[~, min_ind] = min(dqq);
+q_star_curr = qo(:,min_ind);
+q.y = qo;
+q.x = g_basis.x;
+q_star_ini = q_star_curr;
+SSE_curr = f_SSEg_pw(f_basistofunction(g_basis.x,0,g_coef_ini,g_basis,false), ...
+    q,q_star_ini);
+logl_curr = f_logl_pw(f_basistofunction(g_basis.x,0,g_coef_ini,g_basis,false), ...
+    q,q_star_ini,sigma1_ini^2,SSE_curr);
 
-result.g_coef(1,:) = g_coef_ini;
+result.g_coef(1,:,:) = g_coef_ini;
 result.sigma1(1) = sigma1_ini;
-result.SSE(1) = SSE_curr;
+result.q_star(1,:) = q_star_curr;
+result.SSE(1,:) = SSE_curr;
 result.logl(1) = logl_curr;
 
 % update the chain for iter-1 times
@@ -162,18 +182,30 @@ obj = ProgressBar(iter-1, ...
     );
 for m = 2:iter
     % update g
-    [g_coef_curr, ~, SSE_curr, accept, zpcnInd] = f_updateg_pw(g_coef_curr, g_basis, sigma1_curr^2, q1, q2, SSE_curr, @propose_g_coef);
-
+    for ii = 1:N
+        q_tmp = q;
+        q_tmp.y = q_tmp.y(:,ii);
+        [g_coef_curr(:,ii), ~, ~] = f_updateg_pw(g_coef_curr(:,ii), g_basis, sigma1_curr^2, q_tmp, q_star_curr, @propose_g_coef);
+    end
+    SSE_curr = f_SSEg_pw(f_basistofunction(g_basis.x,0,g_coef_curr,g_basis,false), ...
+        q,q_star_curr);
+    % update q_star
+    [q_star_curr, accept, zpcnInd] = f_updateq_pw(g_coef_curr, g_basis, sigma1_curr^2, q, q_star_curr, SSE_curr, @propose_q_star);
+    
     % update sigma1
-    newshape = length(q1.x)/2 + mcmcopts.alpha0;
-    newscale = 1/2 * SSE_curr + mcmcopts.beta0;
+    SSE_curr = f_SSEg_pw(f_basistofunction(g_basis.x,0,g_coef_curr,g_basis,false), ...
+        q,q_star_curr);
+    newshape = length(time)/2 + mcmcopts.alpha0;
+    newscale = 1/2 * sum(SSE_curr) + mcmcopts.beta0;
     sigma1_curr = sqrt(1/gamrnd(newshape,newscale));
-    logl_curr = f_logl_pw(f_basistofunction(g_basis.x, 0, g_coef_curr, g_basis, false), q1, q2, sigma1_curr^2, SSE_curr);
-
+    logl_curr = f_logl_pw(f_basistofunction(g_basis.x,0,g_coef_curr,g_basis,false), ...
+        q,q_star_curr,sigma1_curr^2,SSE_curr);
+    
     % save update to results
-    result.g_coef(m,:) = g_coef_curr;
+    result.g_coef(m,:,:) = g_coef_curr;
+    result.q_star(m,:) = q_star_curr;
     result.sigma1(m) = sigma1_curr;
-    result.SSE(m) = SSE_curr;
+    result.SSE(m,:) = SSE_curr;
     if (mcmcopts.extrainfo)
         result.logl(m) = logl_curr;
         result.accept(m) = accept;
@@ -186,7 +218,7 @@ obj.release();
 % calculate posterior mean of psi
 pw_sim_est_psi_matrix = zeros(length(pw_sim_global_domain_par), length(valid_index));
 for k = 1:length(valid_index)
-    g_temp = f_basistofunction(pw_sim_global_domain_par, 0, result.g_coef(valid_index(k),:).', g_basis, false);
+    g_temp = f_basistofunction(pw_sim_global_domain_par, 0, result.g_coef(valid_index(k),:,3).', g_basis, false);
     psi_temp = f_exp1(g_temp);
     pw_sim_est_psi_matrix(:,k) = psi_temp.y;
 end
@@ -194,8 +226,8 @@ end
 result_posterior_psi_simDomain = f_psimean(pw_sim_global_domain_par, pw_sim_est_psi_matrix);
 
 % resample to same number of points as the input f1 and f2
-result_i = interp1(result_posterior_psi_simDomain.x, result_posterior_psi_simDomain.y, f1.x, 'linear', 'extrap');
-result_posterior_psi.x=f1.x;
+result_i = interp1(result_posterior_psi_simDomain.x, result_posterior_psi_simDomain.y, g_basis.x, 'linear', 'extrap');
+result_posterior_psi.x=g_basis.x;
 result_posterior_psi.y=result_i;
 
 % transform posterior mean of psi to gamma
@@ -286,7 +318,7 @@ end
 end
 
 % function for calculating the next MCMC sample given current state
-function [g_coef, logl, SSE, accept, zpcnInd] = f_updateg_pw(g_coef_curr,g_basis,var1_curr,q1,q2,SSE_curr,propose_g_coef)
+function [g_coef, accept, zpcnInd] = f_updateg_pw(g_coef_curr,g_basis,var1_curr,q1,q2,propose_g_coef)
 g_coef_prop = propose_g_coef(g_coef_curr);
 
 tst = f_exp1(f_basistofunction(g_basis.x,0,g_coef_prop.prop,g_basis, false));
@@ -295,33 +327,55 @@ while (min(tst.y)<0)
     tst = f_exp1(f_basistofunction(g_basis.x,0,g_coef_prop.prop,g_basis, false));
 end
 
-if (SSE_curr == 0)
-    SSE_curr = f_SSEg_pw(f_basistofunction(g_basis.x,0,g_coef_curr,g_basis, false), q1, q2);
-end
+SSE_curr = f_SSEg_pw(f_basistofunction(g_basis.x,0,g_coef_curr,g_basis, false), q1, q2);
 
 SSE_prop = f_SSEg_pw(f_basistofunction(g_basis.x,0,g_coef_prop.prop,g_basis,false), q1, q2);
 
-logl_curr = f_logl_pw(f_basistofunction(g_basis.x,0,g_coef_curr,g_basis,false), q1, q2, var1_curr, SSE_curr);
+logl_ratio = -(SSE_prop - SSE_curr)/(2*var1_curr);
 
-logl_prop = f_logl_pw(f_basistofunction(g_basis.x,0,g_coef_prop.prop,g_basis,false), q1, q2, var1_curr, SSE_prop);
-
-ratio = min(1, exp(logl_prop-logl_curr));
+ratio = min(1, exp(logl_ratio));
 
 u = rand;
 if (u <= ratio)
     g_coef = g_coef_prop.prop;
-    logl = logl_prop;
-    SSE = SSE_prop;
     accept = true;
     zpcnInd = g_coef_prop.ind;
 end
 
 if (u > ratio)
     g_coef = g_coef_curr;
-    logl = logl_curr;
-    SSE = SSE_curr;
     accept = false;
     zpcnInd = g_coef_prop.ind;
+end
+end
+
+% function for calculating the next MCMC sample given current state
+function [q_star, accept, zpcnInd] = f_updateq_pw(g_coef_curr,g_basis,var1_curr,q,q_star_curr,SSE_curr,propose_q_star)
+q_star_prop = propose_q_star(q_star_curr);
+
+if (SSE_curr == 0)
+    SSE_curr = f_SSEg_pw(f_basistofunction(g_basis.x,0,g_coef_curr,g_basis, false), q, q_star_curr);
+end
+
+SSE_prop = sum(f_SSEg_pw(f_basistofunction(g_basis.x,0,g_coef_curr,g_basis,false), q, q_star_prop.prop));
+
+logl_curr = f_logl_pw(f_basistofunction(g_basis.x,0,g_coef_curr,g_basis,false), q, q_star_curr, var1_curr, SSE_curr);
+
+logl_prop = f_logl_pw(f_basistofunction(g_basis.x,0,g_coef_curr,g_basis,false), q, q_star_prop.prop, var1_curr, SSE_prop);
+
+ratio = min(1, exp(logl_prop-logl_curr));
+
+u = rand;
+if (u <= ratio)
+    q_star = q_star_prop.prop;
+    accept = true;
+    zpcnInd = q_star_prop.ind;
+end
+
+if (u > ratio)
+    q_star = q_star_curr;
+    accept = false;
+    zpcnInd = q_star_prop.ind;
 end
 end
 
@@ -335,50 +389,29 @@ end
 %##########################################################################
 % SSEg: sum of sq error= sum over ti of { q1(ti)-{q2,g}(ti) }^2
 % (Eq 11 of JCGS)
-function out = f_SSEg_pw(g, q1, q2)
-obs_domain = q1.x;
-exp1g_temp = f_predictfunction(f_exp1(g), obs_domain, 0);
-pt = [0; bcuL2norm2(obs_domain, exp1g_temp.y)];
-tmp = f_predictfunction(q2, pt, 0);
-vec = (q1.y - tmp.y .* exp1g_temp.y).^2;
-out = sum(vec);
+function out = f_SSEg_pw(g, q, q_star)
+obs_domain = g.x;
+out = zeros(1,size(q.y,2));
+for ii = 1:size(q.y,2)
+    g_tmp = g;
+    g_tmp.y = g_tmp.y(:,ii);
+    exp1g_temp = f_predictfunction(f_exp1(g_tmp), obs_domain, 0);
+    pt = [0; bcuL2norm2(obs_domain, exp1g_temp.y)];
+    q_tmp = q;
+    q_tmp.y = q_tmp.y(:,ii);
+    tmp = f_predictfunction(q_tmp, pt, 0);
+    vec = (tmp.y .* exp1g_temp.y - q_star).^2;
+    out(ii) = sum(vec);
+end
 end
 
-function out = f_logl_pw(g, q1, q2, var1, SSEg)
+function out = f_logl_pw(g, q, q_star, var1, SSEg)
 if (SSEg == 0)
-    SSEg = f_SSEg_pw(g, q1, q2);
+    SSEg = f_SSEg_pw(g, q, q_star);
 end
-n = length(q1.y);
-out = n * log(1/sqrt(2*pi)) - n * log(sqrt(var1)) - SSEg / (2 * var1);
-end
-
-%##########################################################################
-% calculate Q(f), Qinv(q)
-% f,q: function in the form of list$x, list$y
-% fini: f(0)
-% returns Q(f),Qinv(q), function in the form of struct.x, struct.y
-%##########################################################################
-function out = f_Q(f)
-d = f_predictfunction(f, f.x, 1);
-out.x = f.x;
-out.y = sign(d.y) .* sqrt(abs(d.y));
-end
-
-function out = f_Qinv(q, fini)
-result = zeros(1,length(q.x));
-for i = 1:length(result)
-    y = q.y(1:i);
-    x = q.x(1:i);
-    [x,ia,~] = unique(x);
-    [x, ia1] = sort(x);
-    y = y(ia);
-    y = y(ia1);
-    result(i) = trapzCpp(x,(y*abs(y)));
-end
-result = result + fini;
-result(1) = fini;
-out.x = q.x;
-out.y = result;
+N = size(q.y,2);
+n = length(q.x);
+out = N*n * log(1/sqrt(2*pi)) - N*n * log(sqrt(var1)) - sum(SSEg) / (2 * var1);
 end
 
 %##########################################################################
@@ -391,9 +424,12 @@ end
 %##########################################################################
 function out = f_predictfunction(f, at, deriv)
 if (deriv == 0)
-    result = interp1(f.x,f.y,at,'linear','extrap');
+    out.y = zeros(length(at),size(f.y,2));
     out.x = at;
-    out.y = result;
+    for ii = 1:size(f.y,2)
+        result = interp1(f.x,f.y(:,ii),at,'linear','extrap');
+        out.y(:,ii) = result;
+    end
 end
 
 if (deriv == 1)
@@ -402,8 +438,8 @@ if (deriv == 1)
     diffy2 = [diff(fmod); 0];
     diffx1 = [0; diff(at)];
     diffx2 = [diff(at); 0];
-
-
+    
+    
     out.x = at;
     out.y = (diffy2 + diffy1) ./ (diffx2 + diffx1);
 end
@@ -457,11 +493,11 @@ end
 % returns the generated function in the form of struct.x=f.domain, struct.y
 %##########################################################################
 function result = f_basistofunction(f_domain, coefconst, coef, basis, plotf)
-if (size(basis.matrix,2) < length(coef))
+if (size(basis.matrix,2) < size(coef,1))
     error('In f_basistofunction, #coeffients exceeds #basis functions.')
 end
 result.x = basis.x;
-result.y = basis.matrix(:,(1:length(coef))) * coef + coefconst;
+result.y = basis.matrix(:,(1:size(coef,1))) * coef + coefconst;
 
 result = f_predictfunction(result, f_domain, 0);
 if (plotf)
