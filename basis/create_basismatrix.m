e0986bf413925d36e3a0a8aafdb77151905c5f95
J. Derek Tucker <jdtuck@sandia.gov>
2018-12-20 12:15:24 -0700

    added legendre basis calculation

--- basis/create_basismatrix.m
+++ basis/create_basismatrix.m
@@ -1,5 +1,5 @@
 function basismat = create_basismatrix(x, df, degree)
-% CREATE_BASISMATRIX Create Basis Matrix
+% CREATE_BASISMATRIX Create Basis Matrix for BSPLINE basis
 tmp = create_bspline_basis([min(x) max(x)],df,degree);
 rangex   = getbasisrange(tmp);
 x        = linspace(rangex(1),rangex(2),length(x))';
