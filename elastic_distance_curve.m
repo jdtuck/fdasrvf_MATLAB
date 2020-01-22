f954b63226890f7855f1c7161c48dff35c10c665
J. Derek Tucker <jdtuck@sandia.gov>
2020-01-22 09:06:10 -0700

    added curve distance

---
+++ elastic_distance_curve.m
@@ -0,0 +1,47 @@
+function [dy, dx] = elastic_distance_curve(beta1, beta2, closed)
+% ELASTIC_DISTANCE_CURVE Calculates the two elastic distances between two
+% curves
+% -------------------------------------------------------------------------
+% This functions calculates the distances between curves,
+% \eqn{D_y} and \eqn{D_x}, where curve 1 is aligned to curve 2
+%
+% Usage: [dy, dx] = elastic_distance_curve(beta1, beta2)
+%        [dy, dx] = elastic_distance_curve(beta1, beta2, closed)
+%
+% Input:
+% beta1: sample function 1
+% beta2: sample function 1
+% closed: boolean if curve is closed (default = false)
+%
+% Output
+% dy: amplitude distance
+% dx: phase distance
+if nargin < 3
+    closed = false;
+end
+
+N = size(beta1,2);
+a=-calculateCentroid(beta1);
+beta1 = beta1 + repmat(a,1,N);
+a=-calculateCentroid(beta2);
+beta2 = beta2 + repmat(a,1,N);
+q1 = curve_to_q(beta1);
+q2 = curve_to_q(beta2);
+
+% Compute shooting vector from mu to q_i
+[qn_t,~,gam] = Find_Rotation_and_Seed_unique(q1,q2,true,closed);
+[qn_t,~] = Find_Best_Rotation(q1,qn_t);
+
+q1dotq2=InnerProd_Q(q1,qn_t);
+
+% Compute shooting vector
+if q1dotq2>1
+    q1dotq2=1;
+end
+
+dy = acos(q1dotq2);
+
+time1 = linspace(0,1,N);
+binsize = mean(diff(time1));
+psi = sqrt(gradient(gam,binsize));
+dx = acos(trapz(time1,psi));
