58a11fde82e6fee12745d67e3174d321def8776a
J. Derek Tukcer <jdtuck@sandia.gov>
2020-05-27 07:02:55 -0600

    add shape pca

--- fdacurve.m
+++ fdacurve.m
@@ -1,9 +1,9 @@
 classdef fdacurve
     %fdacurve A class to provide registration of curves in R^n using SRVF
-    % -------------------------------------------------------------------------
+    % ---------------------------------------------------------------------
     % This class provides alignment methods for curves in R^n using the
     % SRVF framework
-
+    
     properties
         beta      % (n,T,K) matrix defining n dimensional curve on T samples with K curves
         q         % (n,T,K) matrix defining n dimensional srvf on T samples with K srvfs
@@ -15,6 +15,9 @@ classdef fdacurve
         gams      % warping functions
         v         % shooting vectors
         C         % karcher covariance
+        s         % pca singular values
+        U         % pca singular vectors
+        coef      % pca coefficients
         closed    % closed curve if true
         qun       % cost function
         samples   % random samples
@@ -23,7 +26,7 @@ classdef fdacurve
         scale     % scale
         E         % energy
     end
-
+    
     methods
         function obj = fdacurve(beta, closed, N, scale)
             %fdacurve Construct an instance of this class
@@ -32,13 +35,13 @@ classdef fdacurve
             %   closed: true or false if closed curve
             %   N: resample curve to N points
             %   scale: scale curve to length 1 (true/false)
-
+            
             if nargin < 4
                 scale = true;
             end
             obj.scale = scale;
             obj.closed = closed;
-
+            
             K = size(beta,3);
             n = size(beta,1);
             q = zeros(n,N,K);
@@ -55,10 +58,10 @@ classdef fdacurve
             obj.beta = beta1;
             obj.cent = cent1;
         end
-
+        
         function obj = karcher_mean(obj,option)
             % KARCHER_MEAN Calculate karcher mean of group of curves
-            % -------------------------------------------------------------------------
+            % -------------------------------------------------------------
             % This function aligns a collection of functions using the
             % square-root velocity framework
             %
@@ -75,13 +78,13 @@ classdef fdacurve
             %
             % Output:
             % fdacurve object
-
+            
             if nargin < 2
                 option.parallel = 0;
                 option.closepool = 0;
                 option.MaxItr = 20;
             end
-
+            
             % time warping on a set of functions
             if option.parallel == 1
                 if isempty(gcp('nocreate'))
@@ -100,7 +103,7 @@ classdef fdacurve
                     end
                 end
             end
-
+            
             % Initialize mu as one of the shapes
             shape=1;
             mu=obj.q(:,:,shape);
@@ -115,7 +118,7 @@ classdef fdacurve
             tolv=10^-4;
             told=5*10^-3;
             delta=0.5;
-
+            
             % Compute the Karcher mean
             fprintf('Computing Karcher mean of %d curves in SRVF space...\n',K);
             while iter<=option.MaxItr
@@ -127,7 +130,7 @@ classdef fdacurve
                 if obj.closed
                     obj.basis=findBasisNormal(mu);
                 end
-
+                
                 sumv=zeros(n,T);
                 sumd(1) = Inf;
                 sumd(iter+1)=0;
@@ -135,23 +138,23 @@ classdef fdacurve
                 if option.parallel
                     parfor i=1:K
                         q1=obj.q(:,:,i);
-
+                        
                         % Compute shooting vector from mu to q_i
                         [qn_t,~,gamI] = Find_Rotation_and_Seed_unique(mu,q1,true,obj.closed);
                         if obj.scale
                             qn_t = qn_t/sqrt(InnerProd_Q(qn_t,qn_t));
                         end
                         gamma(:,i) = gamI;
-
+                        
                         q1dotq2=InnerProd_Q(mu,qn_t);
-
+                        
                         % Compute shooting vector
                         if q1dotq2>1
                             q1dotq2=1;
                         end
-
+                        
                         d = acos(q1dotq2);
-
+                        
                         u=qn_t-q1dotq2*mu;
                         normu=sqrt(InnerProd_Q(u,u));
                         if normu>10^-4
@@ -159,37 +162,37 @@ classdef fdacurve
                         else
                             w=zeros(size(qn_t));
                         end
-
+                        
                         % Project to tangent space of manifold to obtain v_i
                         if obj.closed
                             v1(:,:,i)=projectTangent(w,q1,obj.basis);
                         else
                             v1(:,:,i)=w;
                         end
-
+                        
                         sumv=sumv+v1(:,:,i);
                         sumnd_t=sumnd_t+d^2;
                     end
                 else
                     for i=1:K
                         q1=obj.q(:,:,i);
-
+                        
                         % Compute shooting vector from mu to q_i
                         [qn_t,~,gamI] = Find_Rotation_and_Seed_unique(mu,q1,true,obj.closed);
                         if obj.scale
                             qn_t = qn_t/sqrt(InnerProd_Q(qn_t,qn_t));
                         end
                         gamma(:,i) = gamI;
-
+                        
                         q1dotq2=InnerProd_Q(mu,qn_t);
-
+                        
                         % Compute shooting vector
                         if q1dotq2>1
                             q1dotq2=1;
                         end
-
+                        
                         d = acos(q1dotq2);
-
+                        
                         u=qn_t-q1dotq2*mu;
                         normu=sqrt(InnerProd_Q(u,u));
                         if normu>10^-4
@@ -197,59 +200,59 @@ classdef fdacurve
                         else
                             w=zeros(size(qn_t));
                         end
-
+                        
                         % Project to tangent space of manifold to obtain v_i
                         if obj.closed
                             v1(:,:,i)=projectTangent(w,q1,obj.basis);
                         else
                             v1(:,:,i)=w;
                         end
-
+                        
                         sumv=sumv+v1(:,:,i);
                         sumnd_t=sumnd_t+d^2;
                     end
                 end
                 sumd(iter+1) = sumnd_t;
-
+                
                 % Compute average direction of tangent vectors v_i
                 vbar=sumv/K;
                 normvbar(iter)=sqrt(InnerProd_Q(vbar,vbar));
                 normv=normvbar(iter);
-
+                
                 if (normv>tolv) && abs(sumd(iter+1)-sumd(iter))>told
                     % Update mu in direction of vbar
                     mu=cos(delta*normvbar(iter))*mu+sin(delta*normvbar(iter))*vbar/normvbar(iter);
-
+                    
                     % Project the updated mean to the affine (or closed) shape manifold
                     if obj.closed
                         mu = ProjectC(mu);
                     end
-
+                    
                     x=q_to_curve(mu);
                     a=-calculateCentroid(x);
                     betamean=x+repmat(a,1,T);
-
+                    
                 else
                     break
                 end
-
+                
                 iter=iter+1;
-
+                
             end
-
+            
             betan1 = obj.beta;
             qn1 = obj.q;
             if option.parallel
                 parfor i=1:K
                     q1=obj.q(:,:,i);
                     beta1 = betan1(:,:,i);
-
+                    
                     % Compute shooting vector from mu to q_i
                     [~,R,gamI] = Find_Rotation_and_Seed_unique(mu,q1,true,obj.closed);
                     beta1 = R*beta1;
                     beta1n = warp_curve_gamma(beta1,gamI);
                     q1n = curve_to_q(beta1n);
-
+                    
                     % Find optimal rotation
                     [qn1(:,:,i),R] = Find_Best_Rotation(mu,q1n);
                     betan1(:,:,i) = R*beta1n;
@@ -262,19 +265,19 @@ classdef fdacurve
                 for i=1:K
                     q1=obj.q(:,:,i);
                     beta1 = obj.beta(:,:,i);
-
+                    
                     % Compute shooting vector from mu to q_i
                     [~,R,gamI] = Find_Rotation_and_Seed_unique(mu,q1,true,obj.closed);
                     beta1 = R*beta1;
                     beta1n = warp_curve_gamma(beta1,gamI);
                     q1n = curve_to_q(beta1n);
-
+                    
                     % Find optimal rotation
                     [obj.qn(:,:,i),R] = Find_Best_Rotation(mu,q1n);
                     obj.betan(:,:,i) = R*beta1n;
                 end
             end
-
+            
             if option.parallel == 1 && option.closepool == 1
                 if isempty(gcp('nocreate'))
                     delete(gcp('nocreate'))
@@ -286,13 +289,13 @@ classdef fdacurve
             obj.gams = gamma;
             obj.v = v1;
             obj.qun = sumd(1:iter);
-            obj.E = normvbar(1:iter);
-
+            obj.E = normvbar(1:iter-1);
+            
         end
-
+        
         function obj = karcher_cov(obj)
             % KARCHER_COV Calculate karcher covariance
-            % -------------------------------------------------------------------------
+            % -------------------------------------------------------------
             % This function aligns a collection of functions using the
             % square-root velocity framework
             %
@@ -302,9 +305,9 @@ classdef fdacurve
             %
             % Output:
             % fdacurve object
-
+            
             [n,T,K] = size(obj.v);
-
+            
             obj.C=zeros(n*T);
             for i=1:K
                 w=obj.v(:,:,i);
@@ -314,10 +317,49 @@ classdef fdacurve
             end
             obj.C=obj.C/(K-1);
         end
-
+        
+        function obj = shape_pca(obj, no)
+            % SHAPE_PCA Calculate Principal Component Analysis
+            % -------------------------------------------------------------
+            % This function aligns a collection of functions using the
+            % square-root velocity framework
+            %
+            % Usage:  obj.shape_pca()
+            %         obj.shape_pca(10)
+            %
+            % Input:
+            % no: number of principal components (default = 10)
+            %
+            % Output:
+            % fdacurve object
+            if nargin < 2
+                no = 10;
+            end
+            
+            if isempty(obj.C)
+                obj = karcher_cov(obj);
+            end
+            
+            % svd
+            [U1,S,~] = svd(obj.C);
+            ss = diag(S);
+            obj.U = U1(:,1:no);
+            obj.s = ss(1:no);
+            
+            % express shapes as coefficients
+            K = size(obj.beta,3);
+            VM = mean(obj.v,3);
+            x = zeros(no,K);
+            for ii = 1:K
+                tmpv = obj.v(:,:,ii);
+                x(:,ii) = obj.U'*(tmpv(:) - VM(:));
+            end
+            obj.coef = x;
+        end
+        
         function obj = sample_shapes(obj, N, m)
             % SAMPLE_SHAPES Sample shapes from generative model
-            % -------------------------------------------------------------------------
+            % -------------------------------------------------------------
             % This function samples shapes from a generative model using a
             % wrapped normal density on shape pca space
             %
@@ -329,7 +371,7 @@ classdef fdacurve
             %
             % Output:
             % fdacurve object
-
+            
             if nargin < 3
                 N = 10;
                 m = 3;
@@ -345,7 +387,7 @@ classdef fdacurve
                 v1=gradient(p,1/(T-1));
                 len(ii) = sum(sqrt(sum(v1.*v1)))/T;
             end
-
+            
             if (length(unique(len))==1)
                 scale = 1;
             else
@@ -354,9 +396,9 @@ classdef fdacurve
             
             % random warping
             gam_s = randomGamma(obj.gams.',N);
-      
+            
             [U, S, ~] = svd(obj.C);
-
+            
             % number of shapes in calculating exp mapping
             if obj.closed
                 n1 = 10;
@@ -364,10 +406,10 @@ classdef fdacurve
                 n1 = 2;
             end
             epsilon=1/(n1-1);
-
+            
             n = size(obj.beta_mean,1);
             T = size(obj.beta_mean,2);
-
+            
             obj.samples = zeros(n,T,N);
             for i = 1:N
                 v1 = zeros(n,T);
@@ -375,12 +417,12 @@ classdef fdacurve
                     Utemp = reshape(U(:,dir),T,n').';
                     v1 = v1 + randn * sqrt(S(dir,dir))*Utemp;
                 end
-
+                
                 % q = exp_mu(v) using n1 steps
                 q1 = obj.q_mean;
                 for j = 1:n1-1
                     normv = sqrt(InnerProd_Q(v1,v1));
-
+                    
                     if normv < 1e-4
                         q2 = mu;
                     else
@@ -389,14 +431,14 @@ classdef fdacurve
                             q2 = ProjectC(q2);
                         end
                     end
-
+                    
                     % parallel translate tangent vector
                     v1=v1-2*InnerProd_Q(v1,q2)/InnerProd_Q(q1+q2,q1+q2)*(q1+q2);
-
+                    
                     q1 = q2;
                 end
-
-                % random scale 
+                
+                % random scale
                 if (length(unique(len))>1)
                     scale = random(pd);
                 end
@@ -410,9 +452,9 @@ classdef fdacurve
             end
             
             
-
+            
         end
-
+        
         function plot(obj, color)
             % plot plot curve mean results
             % -------------------------------------------------------------------------
@@ -437,7 +479,7 @@ classdef fdacurve
                 title('Curves')
             end
             axis equal ij off;
-
+            
             if (~isempty(obj.gams))
                 figure(2); clf
                 if n == 2
@@ -449,7 +491,7 @@ classdef fdacurve
                 end
                 title('Karcher Mean')
                 axis equal ij off;
-
+                
                 figure(3); clf;
                 M = size(obj.beta,2);
                 plot((0:M-1)/(M-1), obj.gams, 'linewidth', 1);
@@ -479,6 +521,32 @@ classdef fdacurve
                 axis equal ij off;
             end
         end
+        
+        function plot_pca(obj)
+            figure(5)
+            plot(obj.s)
+            title('Singular Values')
+            
+            % plot principal modes of variability
+            VM = mean(obj.v,3);
+            VM = VM(:);
+            for j = 1:4
+                figure(20+j); clf; hold on;
+                for i=1:10
+                    tmp = VM + 0.5*(i-5)*sqrt(ss(j))*obj.U(:,j);
+                    v1 = reshape(tmp,2,41);
+                    q2n = ElasticShooting(obj.mu,v1);
+                    
+                    p = q_to_curve(q2n);
+                    if i == 5
+                        plot(0.2*i + p(1,:),p(2,:), 'k','LineWidth',3);
+                    else
+                        plot(0.2*i + p(1,:),p(2,:),'LineWidth',2);
+                    end
+                    axis equal ij off;
+                end
+            end
+        end
     end
 end
 
@@ -486,4 +554,9 @@ function plot3var(x,y,z,c,lwd)
 surface('XData',[x(:) x(:)], 'YData',[y(:) y(:)], 'ZData',[z(:) z(:)], ...
     'CData',[c(:) c(:)], 'FaceColor','none', 'EdgeColor','interp', ...
     'Marker','none','linew',lwd)
+end
+
+function plot_curve2(beta)
+plot(beta(1,:),beta(2,:),'LineWidth',1.5);
+axis equal ij off;
 end
