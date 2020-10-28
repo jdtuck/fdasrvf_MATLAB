a27c15ce642951a6fd801e506a749a55dd39f3a8
J. Derek Tukcer <jdtuck@sandia.gov>
2020-10-28 13:18:58 -0600

    expose method to elastic_distance

--- elastic_distance.m
+++ elastic_distance.m
@@ -1,4 +1,4 @@
-function [dy, dx] = elastic_distance(f1, f2, time, lambda)
+function [dy, dx] = elastic_distance(f1, f2, time, lambda, method)
 % ELASTIC_DISTANCE Calculates the two elastic distances between two
 % functions
 % -------------------------------------------------------------------------
@@ -13,16 +13,23 @@ function [dy, dx] = elastic_distance(f1, f2, time, lambda)
 % f2: sample function 1
 % time: vector of size \eqn{N} describing the sample points
 % lambda controls amount of warping (default = 0)
+% method: controls which optimization method (default="DP") options are
+% Dynamic Programming ("DP"), Coordinate Descent ("DP2"), and Riemannian BFGS
+% ("RBFGSM")
 %
 % Output
 % dy: amplitude distance
 % dx: phase distance
 if nargin < 4
-  lambda = 0;
+    lambda = 0;
+    method = 'DP';
+elseif nargin < 5
+    method = 'DP';
 end
+
 q1 = f_to_srvf(f1,time);
 q2 = f_to_srvf(f2,time);
-gam = optimum_reparam(q1,q2,time.',lambda,'DP');
+gam = optimum_reparam(q1,q2,time.',lambda,method);
 fw = warp_f_gamma(f2,gam,time.');
 qw = f_to_srvf(fw.',time);
 dy = sqrt(trapz(time,(q1-qw).^2));
