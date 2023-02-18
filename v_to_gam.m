c6cd11a639642bcc3d0b1d6adfd2a5ed81dbd52a
J. Derek Tucker <jdtuck@sandia.gov>
2023-02-18 16:02:53 -0700

    bugfix

--- v_to_gam.m
+++ v_to_gam.m
@@ -2,12 +2,23 @@ function gam = v_to_gam(vec)
     % v_to_gam Convert shooting vectors to warping functions
     % -----------------------------------------------------_
 
-    [n,T] = size(vec);
-    time = linspace(0,1,T);
-
-    gam = zeros(n,T);
-    for i=1:n
-        psi = exp_map(mu,vec(i,:));
+    if size(vec,2) > 1
+        [n,T] = size(vec);
+        time = linspace(0,1,T);
+    
+        gam = zeros(n,T);
+        mu = ones(1, T);
+        for i=1:n
+            psi = exp_map(mu,vec(i,:));
+            gam_tmp = cumtrapz(time,psi.^2);
+            gam(i,:) = (gam_tmp-min(gam_tmp))/(max(gam_tmp)-min(gam_tmp));
+        end
+    else
+        T = length(vec);
+        time = linspace(0,1,T);
+        
+        mu = ones(1,T);
+        psi = exp_map(mu,vec);
         gam_tmp = cumtrapz(time,psi.^2);
-        gam(i,:) = (gam_tmp-min(gam_tmp))/(max(gam_tmp)-min(gam_tmp));
+        gam = (gam_tmp-min(gam_tmp))/(max(gam_tmp)-min(gam_tmp));
     end
