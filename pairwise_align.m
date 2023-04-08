832207f1f91af56f5dba2a87466a9dfddd3b0381
Derek Tucker <jdtuck@sandia.gov>
2023-04-08 06:35:04 -0600

    add penalty

--- pairwise_align.m
+++ pairwise_align.m
@@ -1,4 +1,4 @@
-function [f2_align, gam] = pairwise_align(f1,f2,time)
+function [f2_align, gam] = pairwise_align(f1,f2,time,lam)
 % PAIRWISE_ALIGN Align two functions
 % -------------------------------------------------------------------------
 % This function aligns two functions using SRSF framework. It will align f2
@@ -10,13 +10,20 @@ function [f2_align, gam] = pairwise_align(f1,f2,time)
 % f1: vector defining M samples of function 1
 % f2: vector defining M samples of function 2
 % time: time vector of length M
+% lambda: penalty (default = 0)
 %
 % Output:
 % f2_align: aligned f2
 % gam: warping function
+arguments
+    f1 double
+    f2 double
+    time double
+    lam = 0
+end
 q1 = f_to_srvf(f1,time);
 q2 = f_to_srvf(f2,time);
 
-gam = optimum_reparam(q1,q2,time,0,'DP1',0.0,0.0,0.0);
+gam = optimum_reparam(q1,q2,time,lam,'DP1',0.0,0.0,0.0);
 
 f2_align = warp_f_gamma(f2,gam,time);
