ad006777247b111c5486b7bbd5d8d90d26f41406
Derek Tucker <jdtuck@sandia.gov>
2023-12-21 04:46:39 -0700

    remove c++ files

--- gropt/Matlab/ForCpp/MTestEucFrechetMean.m
+++
@@ -1,8 +0,0 @@
-function MTestEucFrechetMean()
-    dim = 10;
-    num = 20;
-    Data = randn(dim, num);
-    Weight = (num : -1 : 1)';
-    Xinitial = randn(dim, 1);
-    TestEucFrechetMean(Data, Weight, Xinitial);
-end
--- gropt/Matlab/ForCpp/MTestEucQuadratic.m
+++
@@ -1,7 +0,0 @@
-function MTestEucQuadratic()
-    dim = 10;
-    M = randn(dim, dim);
-    M = M * M';
-    Xinitial = randn(dim, 1);
-    TestEucQuadratic(M, Xinitial);
-end
