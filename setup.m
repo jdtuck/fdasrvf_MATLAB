5deeb80c046d7d635dff0769bee0e994b9a0ce3a
Derek Tucker <jdtuck@sandia.gov>
2025-10-10 16:30:58 -0600

    update for new xcode

--- setup.m
+++ setup.m
@@ -8,10 +8,10 @@ mex -outdir DP/ DP/DynamicProgrammingQ.c
 
 % Bayesian
 fprintf('Compiling Bayesian files...\n');
-mex -outdir armadillo_cpp/ armadillo_cpp/bcalcY.cpp
-mex -outdir armadillo_cpp/ armadillo_cpp/bcuL2norm2.cpp 
-mex -outdir armadillo_cpp/ armadillo_cpp/trapzCpp.cpp
-mex -outdir armadillo_cpp/ armadillo_cpp/border_l2norm.cpp
+mex -ld_classic -outdir armadillo_cpp/ armadillo_cpp/bcalcY.cpp
+mex -ld_classic -outdir armadillo_cpp/ armadillo_cpp/bcuL2norm2.cpp 
+mex -ld_classic -outdir armadillo_cpp/ armadillo_cpp/trapzCpp.cpp
+mex -ld_classic -outdir armadillo_cpp/ armadillo_cpp/border_l2norm.cpp
 
 % minFunc (lbfgs optimizer)
 fprintf('Compiling minFunc files...\n');
@@ -28,8 +28,8 @@ mex -outdir mlogit_warp/ mlogit_warp/mlogit_warp.c mlogit_warp/mlogit_warp_grad.
 fprintf('Compiling rbfgs files...\n');
 if ispc
     libDir = fullfile(matlabroot,"/extern/lib/win64/microsoft");    %Path to LAPAC and BLAS libs
-    mexCmd = sprintf('mex -outdir armadillo_cpp/ armadillo_cpp/c_rlbfgs.cpp -L"%s" -lmwlapack -lmwblas', libDir);
+    mexCmd = sprintf('mex LINKFLAGS=''$LINKFLAGS -ld_classic'' -outdir armadillo_cpp/ armadillo_cpp/c_rlbfgs.cpp -L"%s" -lmwlapack -lmwblas', libDir);
     eval(mexCmd);
 else
-    mex -outdir armadillo_cpp/ armadillo_cpp/c_rlbfgs.cpp -llapack -lblas
+    mex -ld_classic -outdir armadillo_cpp/ armadillo_cpp/c_rlbfgs.cpp -llapack -lblas
 end
