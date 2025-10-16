dfd1c3555c8f4be426babc394cf6ddcd326eeb9d
Derek Tucker <jdtuck@sandia.gov>
2025-10-16 15:01:55 -0600

    update contents

--- Contents.m
+++ Contents.m
@@ -4,44 +4,58 @@
 %   align_fPCA                    - Group-wise function alignment and PCA Extractions
 %   ampbox                        - ampbox Produce amplitude box plots
 %   Basis_Normal_A                - Find Normal Basis
-%   bootstrap_tol                 - Boostrapped tolerance bounds
+%   bootTB                        - Boostrapped tolerance bounds
 %   calculateCentroid             - Calculate centroid of curve
 %   create_basismatrix            - Create Basis Matrix
 %   cumsimps                      - Cumulative Simpson's numerical integration.
 %   cumtrapzmid                   - Cummulative Trapezodial Integration from mid point
-%   curve_to_q                    - Convert curve to Square-Root Velocity Function
-%   elastic_distance              - Calculates the two elastic distances between two
+%   curve_to_q                    - Convert curve to Square-Root Velocity Function        
+%   elastic_depth                 - compute elastic depth   
+%   elastic_distance              - Calculates the two elastic distances between two functions
+%   elastic_distance_curve        - Calculates the elastic distance between two curves
 %   elastic_logistic              - elastic_logistic A class to provide SRVF logistic regression
-%   elastic_mlogistic             - elastic_mlogistic A class to provide wSRVF multionomial logistic
+%   elastic_lcpr_regression       - elastic logistic principal component regression
+%   elastic_mlogistic             - elastic_mlogistic A class to provide SRVF multionomial logistic
+%   elastic_mlpcr_regression      - elastic multinmoical principal component regression
 %   elastic_regression            - elastic_regression A class to provide SRVF regression
+%   elastic_pcr_regression        - elastic principal component regression
 %   exp_map                       - Exponential Map
 %   f_to_srvf                     - Convert function to Square-Root Velocity Function
 %   fdacurve                      - fdacurve A class to provide registration of curves in R^n using SRVF
 %   fdahpca                       - fdahpca A class to provide horizontal fPCA
+%   fdahpns                       - fdahpns A class to pronivde horizontal fPNS
+%   fdajpca                       - fdajpca A class to provide joint fPCA
 %   fdakma                        - fdakma A class to provide a kmeans clustering and alignment
 %   fdavpca                       - fdavpca A class to provide vertical fPCA
 %   fdawarp                       - fdawarp A class to provide alignment of functional data using SRVF
 %   Find_Best_Rotation            - Find best rotation between two curves
 %   Find_Rotation_and_Seed_unique - find roation and seed of two curves
+%   gam_to_h                      - convert warping function to h space
+%   gam_to_psi                    - convert warping fucntion to hilbert sphere
+%   gam_to_v                      - convert warping function to tangent space on hilbert sphere
 %   geodesic_sphere_Full          - Calculates geodesic on sphere
 %   Gram_Schmidt                  - orthonormalize basis
+%   h_to_gam                      - convert h to warping function
 %   inner_product                 - Functional inner product
 %   InnerProd_Q                   - Calculate inner product of two SRVFs
 %   inv_exp_map                   - Inverse Exponential Map
 %   invertGamma                   - Invert Warping Function
 %   L2norm                        - L2 Functional Norm
 %   optimum_reparam               - Calculates Warping for two SRVFs
+%   optimum_reparam_curve         - Calculates warping for two curve SRVFs
 %   outlier_detection             - Outlier Detection
 %   pairwise_align                - Align two functions
 %   pairwise_align_bayes          - PAIRWISE_ALIGN Align two functions using Bayesian method
 %   pairwise_align_curves         - registers to curves
 %   Parallel_Transport_C          - PARALLEL_TRANSPORT Parallel transport vector
 %   Path_Plot                     - Plot geodesic path
+%   pcaTB                         - functional principal component tolerance bound generation
 %   phbox                         - phbox Construct phase box plots
 %   progress                      - Wrapper class to provide an iterator object for loop creation
 %   ProgressBar                   - A class to provide a convenient and useful progress bar
 %   Project_Tangent               - Project tangent
 %   ProjectC                      - Project closed curve
+%   psi_to_gam                    - Hilbert sphere to warping function
 %   q_to_curve                    - Convert SRVF to curve
 %   ReSampleCurve                 - Resample curve to have N points
 %   rgam                          - Generate random warping functions
@@ -58,6 +72,7 @@
 %   srvf_to_f                     - Convert SRSF to f
 %   Translation_Boxplot           - Translation Boxplot
 %   updateParallel                - Update function when ProgressBar is used in parallel setup
+%   v_to_gam                      - tangent space to warping function
 %   varycolor                     - Produces colors with maximum variation on plots with multiple
 %   warp_curve_gamma              - Warp curve by gamma
 %   warp_f_gamma                  - Warp Function by gamma
--- example.m
+++ example.m
@@ -2,8 +2,6 @@ setup_paths
 
 load data/simu_data.mat
 
-
 obj = fdawarp(f, t);
-obj = obj.ppd();
 obj = obj.time_warping();
 obj.plot()
