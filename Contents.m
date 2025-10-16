59e79389b10f2140ea2069b0b060a5f471f2589c
Derek Tucker <jdtuck@sandia.gov>
2025-10-16 16:05:01 -0600

    reorganize

--- Contents.m
+++ Contents.m
@@ -3,12 +3,9 @@
 % Files
 %   align_fPCA                    - Group-wise function alignment and PCA Extractions
 %   ampbox                        - ampbox Produce amplitude box plots
-%   Basis_Normal_A                - Find Normal Basis
 %   bootTB                        - Boostrapped tolerance bounds
 %   calculateCentroid             - Calculate centroid of curve
 %   create_basismatrix            - Create Basis Matrix
-%   cumsimps                      - Cumulative Simpson's numerical integration.
-%   cumtrapzmid                   - Cummulative Trapezodial Integration from mid point
 %   curve_to_q                    - Convert curve to Square-Root Velocity Function        
 %   elastic_depth                 - compute elastic depth   
 %   elastic_distance              - Calculates the two elastic distances between two functions
@@ -28,16 +25,11 @@
 %   fdakma                        - fdakma A class to provide a kmeans clustering and alignment
 %   fdavpca                       - fdavpca A class to provide vertical fPCA
 %   fdawarp                       - fdawarp A class to provide alignment of functional data using SRVF
-%   Find_Best_Rotation            - Find best rotation between two curves
-%   Find_Rotation_and_Seed_unique - find roation and seed of two curves
 %   gam_to_h                      - convert warping function to h space
 %   gam_to_psi                    - convert warping fucntion to hilbert sphere
 %   gam_to_v                      - convert warping function to tangent space on hilbert sphere
 %   geodesic_sphere_Full          - Calculates geodesic on sphere
-%   Gram_Schmidt                  - orthonormalize basis
 %   h_to_gam                      - convert h to warping function
-%   inner_product                 - Functional inner product
-%   InnerProd_Q                   - Calculate inner product of two SRVFs
 %   inv_exp_map                   - Inverse Exponential Map
 %   invertGamma                   - Invert Warping Function
 %   L2norm                        - L2 Functional Norm
@@ -47,14 +39,9 @@
 %   pairwise_align                - Align two functions
 %   pairwise_align_bayes          - PAIRWISE_ALIGN Align two functions using Bayesian method
 %   pairwise_align_curves         - registers to curves
-%   Parallel_Transport_C          - PARALLEL_TRANSPORT Parallel transport vector
 %   Path_Plot                     - Plot geodesic path
 %   pcaTB                         - functional principal component tolerance bound generation
 %   phbox                         - phbox Construct phase box plots
-%   progress                      - Wrapper class to provide an iterator object for loop creation
-%   ProgressBar                   - A class to provide a convenient and useful progress bar
-%   Project_Tangent               - Project tangent
-%   ProjectC                      - Project closed curve
 %   psi_to_gam                    - Hilbert sphere to warping function
 %   q_to_curve                    - Convert SRVF to curve
 %   ReSampleCurve                 - Resample curve to have N points
@@ -62,7 +49,6 @@
 %   setup                         - setup environment
 %   setup_paths                   - setup paths
 %   ShiftF                        - Shifts curve starting point by tau
-%   simps                         - Simpson's numerical integration.
 %   simul_align                   - Align two functions by simultaneous reparameterization
 %   simul_gam                     - Align two function using simultaneous alignments.
 %   smooth_data                   - Smooth Functions
@@ -71,9 +57,7 @@
 %   SqrtMedian                    - SRVF transform of warping functions calculates median
 %   srvf_to_f                     - Convert SRSF to f
 %   Translation_Boxplot           - Translation Boxplot
-%   updateParallel                - Update function when ProgressBar is used in parallel setup
 %   v_to_gam                      - tangent space to warping function
-%   varycolor                     - Produces colors with maximum variation on plots with multiple
 %   warp_curve_gamma              - Warp curve by gamma
 %   warp_f_gamma                  - Warp Function by gamma
 %   warp_q_gamma                  - WARP_F_GAMMA Warp SRVF by gamma
