4f47c2dd836e44b21b862572b888ee52bd114ec3
Derek Tucker <jdtuck@sandia.gov>
2025-10-16 17:09:40 -0600

    change default

--- test_package.m
+++ test_package.m
@@ -3,7 +3,7 @@ setup_paths
 load data/simu_data.mat
 
 %% Test Distance and Gamma Calc
-[dy,dx] = elastic_distance(f(:,1),f(:,1),t);
+[dy,dx] = elastic_distance(f(:,1),f(:,1),t, 0, 'DP1');
 assert(dy < 1e-12, 'Amp distance not near 0')
 assert(dx < 1e-7, 'Phase distance not near 0')
 
@@ -11,7 +11,7 @@ assert(dx < 1e-7, 'Phase distance not near 0')
 M = 101;
 q1 = sin(linspace(0,2*pi,M));
 timet = linspace(0,1,M);
-gam = optimum_reparam(q1', q1', timet'); 
+gam = optimum_reparam(q1', q1', timet', 0, 'DP1'); 
 q1a = warp_f_gamma(q1, gam, timet); 
 assert(sum(q1(:)-q1a(:))==0,'Warping Not Identity')
 
@@ -19,6 +19,6 @@ assert(sum(q1(:)-q1a(:))==0,'Warping Not Identity')
 M = 101;
 q1 = sin(linspace(0,2*pi,M));
 timet = linspace(0,1,M);
-gam = optimum_reparam(q1', q1', timet'); 
+gam = optimum_reparam(q1', q1', timet', 0, 'DP1'); 
 q1a = warp_q_gamma(q1, gam, timet); 
 assert(sum(q1(:)-q1a(:))<=1e-15,'Warping Not Identity')
