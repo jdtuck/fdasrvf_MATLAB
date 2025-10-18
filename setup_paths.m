72a2874781013a745eb6e61380049fe2a430906a
Derek Tucker <jdtuck@sandia.gov>
2025-10-18 13:33:06 -0600

    clean up paths

--- setup_paths.m
+++ setup_paths.m
@@ -1,12 +1,9 @@
 % setup paths
 addpath(genpath('minFunc'))
-addpath(genpath('DP'))
-addpath(genpath('armadillo_cpp'))
+addpath(genpath('mex'))
 addpath(genpath('bspline_tools'))
 addpath(genpath('basis'))
 addpath(genpath('data'))
-addpath(genpath('gropt'))
-addpath(genpath('mlogit_warp'))
 addpath(genpath('util'))
 addpath(genpath('manifold'))
 addpath(genpath('PNS'))
