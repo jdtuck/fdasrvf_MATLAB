16b6f093721d0b1c9d83f613d411a8af6390fb9e
Derek Tucker <jdtuck@sandia.gov>
2025-10-18 13:30:47 -0600

    clean up mex

--- buildfile.m
+++ buildfile.m
@@ -1,12 +1,17 @@
 function plan = buildfile
 import matlab.buildtool.tasks.CodeIssuesTask
 import matlab.buildtool.tasks.TestTask
+import matlab.buildtool.tasks.CleanTask
+import matlab.buildtool.tasks.MexTask
 
 % Create a plan from task functions
 plan = buildplan(localfunctions);
 
 % Add a task to identify code issues
 plan("check") = CodeIssuesTask;
+
+% Add a task to delete outputs and traces
+plan("clean") = CleanTask;
 
 % Add a task to run tests
 plan("test") = TestTask;
@@ -15,7 +20,7 @@ plan("test") = TestTask;
 plan.DefaultTasks = "archive";
 
 % Make the "archive" task dependent on the "check" and "test" tasks
-plan("archive").Dependencies = ["check" "test"];
+plan("archive").Dependencies = ["clean" "check" "test"];
 end
 
 function archiveTask(~)
